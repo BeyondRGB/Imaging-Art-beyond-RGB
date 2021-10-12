fbeded72cf5e3ebbb95081a51f0904e60e943981
tfk8939 <tfk8939@rit.edu>
2021-10-12 17:21:26 -0400

    Added json parsing lybrary

--- library.h
+++ library.h
@@ -1,8 +1,10 @@
 #ifndef BEYOND_RGB_BACKEND_LIBRARY_H
 #define BEYOND_RGB_BACKEND_LIBRARY_H
+using namespace std;
+#include <iostream>
 
 void hello();
-
+void testJson(const char *json);
 void process();
 
 #endif //BEYOND_RGB_BACKEND_LIBRARY_H
