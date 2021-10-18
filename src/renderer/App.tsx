eb2fa362f983d67c2f67a643ae40b51600e3c57f
pxd5295 <pxd5295@rit.edu>
2021-10-18 16:05:50 -0400

    Basic responsive layout

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import _ from 'lodash';
+import './styles/App.css'
 
 function App(): JSX.Element {
-  console.log("This is React Buddy!");
-  
   return (
-    <div>Hello From React!</div>
+    <div className="App">
+      {StartPage()}
+    </div>
+  )
+}
+
+function StartPage(): JSX.Element {
+  return (
+    <div className="StartPage">
+      <div className="top-nav">
+        Nav
+      </div>
+      <div className="side-menu">
+        Menu
+        {_.range(10).map((value, index) => 
+          <li>{index}</li>
+        )}
+      </div>
+      <div className="start-buttons">
+        Buttons
+        <button>Process</button>
+        <button>View</button>
+      </div>
+    </div>
   )
 }
 
