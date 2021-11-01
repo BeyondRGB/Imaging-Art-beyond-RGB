ca7fb3f839980b09a4013b91be8d7adbb9b4cf6a
tfk8939 <tfk8939@rit.edu>
2021-11-01 17:27:58 -0400

    set up websoceted pipeline

---
+++ backend/src/pipeline.cpp
@@ -0,0 +1,48 @@
+#include "pipeline.h"
+
+Pipeline::Pipeline(server* s, websocketpp::connection_hdl hdl, message_ptr msg, int pipelineNumber) {
+    server_m = s;
+    connectionHandle_m = hdl;
+    msg_m = msg;
+    num_m = pipelineNumber;
+};
+
+void Pipeline::callback(string msg) {
+    msg = "{pipeline(" + std::to_string(num_m) + "):" + msg + "}";
+    cout << "MSG: " << msg << endl;
+    server_m->send(connectionHandle_m, msg, msg_m->get_opcode());
+
+};
+
+shared_ptr<ImgProcessingComponent> Pipeline::pipelineSetup() {
+    //Set up PreProcess components
+    vector<shared_ptr<ImgProcessingComponent>> pre_process_components;
+    pre_process_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new RawImageReader()));
+    pre_process_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new ChannelSelector()));
+    pre_process_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new BitDepthScalor()));
+    pre_process_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new DarkCurrentCorrector()));
+    pre_process_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new FlatFeildor()));
+    pre_process_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new PixelRegestor()));
+    //Set up Calibration components
+    vector<shared_ptr<ImgProcessingComponent>> calibration_components;
+    calibration_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new ColorManagedCalibrator()));
+    calibration_components.push_back(static_cast<const shared_ptr <ImgProcessingComponent>>(new SpectralCalibrator()));
+
+    auto* process = new PreProcessor(pre_process_components);
+    auto* calibrator = new ImageCalibrator(calibration_components);
+
+    vector<shared_ptr<ImgProcessingComponent>> img_process_components;
+    img_process_components.push_back(shared_ptr<ImgProcessingComponent>(process));
+    img_process_components.push_back(shared_ptr<ImgProcessingComponent>(calibrator));
+
+    ImgProcessingComponent* processor = new ImageProcessor(img_process_components);
+    return shared_ptr<ImgProcessingComponent>(processor);
+
+};
+
+void Pipeline::executePipeline() {
+    callback("I got your msg");
+    shared_ptr<ImgProcessingComponent> pipeline = pipelineSetup();
+    pipeline->execute(std::bind(&Pipeline::callback, this, placeholders::_1));
+
+};
---
+++ backend/src/pipeline.h
@@ -0,0 +1,55 @@
+#ifndef PIPELINE_H
+#define PIPELINE_H
+
+#include "image_processing/header/BitDepthScalor.h"
+#include "image_processing/header/ChannelSelector.h"
+#include "image_processing/header/ColorManagedCalibrator.h"
+#include "image_processing/header/DarkCurrentCorrector.h"
+#include "image_processing/header/FlatFeildor.h"
+#include "image_processing/header/ImageCalibrator.h"
+#include "image_processing/header/ImageProcessor.h"
+#include "image_processing/header/ImgProcessingComponent.h"
+#include "image_processing/header/PixelRegestor.h"
+#include "image_processing/header/PreProcessor.h"
+#include "image_processing/header/RawImageReader.h"
+#include "image_processing/header/SpectralCalibrator.h"
+
+#include <iostream>
+using namespace std;
+
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_CPP11_THREAD_
+#define _WEBSOCKETPP_CPP11_STRICT_ 
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+// pull out the type of messages sent by our config
+typedef server::message_ptr message_ptr;
+
+class Pipeline{
+
+
+private:
+	server* server_m = NULL;
+	websocketpp::connection_hdl connectionHandle_m;
+	message_ptr msg_m;
+	int num_m;
+
+	void callback(string msg);
+	shared_ptr<ImgProcessingComponent> pipelineSetup();
+
+public:
+	Pipeline(server* s, websocketpp::connection_hdl hdl, message_ptr msg, int count);
+	void executePipeline();
+
+
+};
+
+
+#endif // !PIPELINE_H
+
+
+
+	
