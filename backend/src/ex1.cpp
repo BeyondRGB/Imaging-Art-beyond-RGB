a677ab5fcf107d2205d84cb5fe2179da99e2a5fe
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2021-11-16 11:43:37 -0500

    Successful *default* conversion of RAW to TIFF.
    I still need to disable all processing options except for demosaicing.

--- backend/src/ex1.cpp
+++ backend/src/ex1.cpp
@@ -6,41 +6,46 @@ extern "C" {
 //#include <libraw/libraw.h>
 
 void find_maxs(LibRaw* imr);
-void write_tiff(LibRaw* imr);
+void write_tiff(uchar* im, int width, int height);
+void write_tiff2(LibRaw* imr);
+void raw_testing(LibRaw* im);
 
 void process_image(char *file) {
 
 	printf("LibRaw started.\r\n");
 
-	// Let us create an image processor
-	LibRaw imr;
+	/* Create an image processor. */
+	LibRaw im;
 
-	// Open the file and read the metadata
-	imr.open_file(file);
+	/* Open the file. */
+	im.open_file(file);
 
-	// Let us unpack the image
-	imr.unpack();
+	/* Read meta data if needed. */
 
-	// Convert from imgdata.rawdata to imgdata.image:
-	imr.raw2image();
+	/* Unpack raw data into structures for processing. */
+	im.unpack();
+
+	/* Call dcraw_process() with default settings. */
+	int ec = 0;
+	im.dcraw_process();
+	libraw_processed_image_t* procIm = im.dcraw_make_mem_image(&ec);
 
 	printf("LibRaw finished.\r\n");
+	printf("procIm data size: %u\r\n", procIm->data_size);
+	printf("procIm channels: %hu\r\n", procIm->colors);
+	printf("procIm byte or short (8 or 16 bits): %hu\r\n", procIm->bits);
 
-	//find_maxs(&imr);
-	write_tiff(&imr);
+	/* Pass the processed bitmap to libtiff for writing. */
+	write_tiff((uchar*) procIm->data, procIm->width, procIm->height);
+
+	im.dcraw_clear_mem(procIm);
 
 	// Finally, let us free the image processor for work with the next image
-	imr.recycle();
+	im.recycle();
 }
 
-
-void write_tiff(LibRaw* imr) {
-	int width = imr->imgdata.sizes.iwidth;
-	int height = imr->imgdata.sizes.iheight;
-
+void write_tiff(uchar* im, int width, int height) {
 	int samples_per_pixel = 3; // Number of channels
-	//int image_row_len = width * samples_per_pixel;
-	//int image_row_bytes = image_row_len * 2;
 
 	printf("Width: %d\r\nHeight: %d\r\n", width, height);
 
@@ -48,12 +53,17 @@ void write_tiff(LibRaw* imr) {
 
 	TIFFSetField(img_out, TIFFTAG_IMAGEWIDTH, width);
 	TIFFSetField(img_out, TIFFTAG_IMAGELENGTH, height);
-	TIFFSetField(img_out, TIFFTAG_SAMPLESPERPIXEL, samples_per_pixel);
-	TIFFSetField(img_out, TIFFTAG_BITSPERSAMPLE, 16);
 
+	/* Set the number of channels. */
+	TIFFSetField(img_out, TIFFTAG_SAMPLESPERPIXEL, samples_per_pixel);
+
+	/* We'll do 16 bits, dcraw_process() defaults to 8 bits. */
+	TIFFSetField(img_out, TIFFTAG_BITSPERSAMPLE, 8);
+	
+	/* Image rotation. */
 	TIFFSetField(img_out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
 
-	/* Write all channel data in one array instead of separating the channels. */
+	/* Write all channel data in one array (a bitmap) instead of separating the channels. */
 	TIFFSetField(img_out, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
 
 	/* Indicate that the image is RGB. We'll need to do grayscale, perhaps 
@@ -77,68 +87,32 @@ void write_tiff(LibRaw* imr) {
 	 * of bytes for one physical pixel row of the image, and the number of
 	 * strips should equal the height of the image. */
 	tmsize_t scanline_size = TIFFScanlineSize(img_out);
-	unsigned short* sample_row = (unsigned short *) _TIFFmalloc(scanline_size);
+	uchar* sample_row = (uchar *) _TIFFmalloc(scanline_size);
 	printf("Scanline Size: %lld\r\n", scanline_size);
-	
+
 	int col, x, y, i;
 	for( y = 0; y < height; y++) {
 		col = 0;
 		for( x = 0; x < width; x++) {
-			i = x * y;
-			/* 12 bit -> 16 bit: 2^16/2^12 = 16*/
-			/* Red    */ sample_row[col++] = imr->imgdata.image[i][0] * 16;
-			/* Green1 */ sample_row[col++] = imr->imgdata.image[i][1] * 16;
-			/* Blue   */ sample_row[col++] = imr->imgdata.image[i][2] * 16;
-			/* Green2 */ //sample_row[col++] = imr->imgdata.image[i][3];
+			i = (y * width * samples_per_pixel) + (x * samples_per_pixel);
+			/* R */ sample_row[col++] = im[i + 0];
+			/* G */ sample_row[col++] = im[i + 1];
+			/* B */ sample_row[col++] = im[i + 2];
 		}
 		if( y % 100 == 0 )
-			printf("Row% 4d:% 5d bytes\r\n", y, col * 2);
+			printf("Row% 4d:% 5d bytes  |  i: %d\r\n", y, col, i);
 		if (TIFFWriteScanline(img_out, sample_row, y, 0) < 0)
     		break;
 
 	}
-	printf("Row% 4d:% 5d bytes\r\n", y, col * 2);
+	printf("Row% 4d:% 5d bytes  |  i: %d\r\n", y, col, i);
 
 	TIFFClose(img_out);
 	if (sample_row)
     	_TIFFfree(sample_row);
 
-}
+	printf("TIFF finished.\r\n");
 
-void find_maxs(LibRaw* imr) {/*
-	int width = imr->imgdata.sizes.iwidth;
-	int height = imr->imgdata.sizes.iheight;
-	int widthr = imr->imgdata.sizes.raw_width;
-	int heightr = imr->imgdata.sizes.raw_height;
-
-	int x, y;
-    unsigned long i, ir, c;
-
-	unsigned short max = 0, min = 0xFFFF, maxr = 0, minr = 0xFFFF, p;
-	
-	printf("Raw:\r\n");
-    for( y = 0; y < heightr; y++) {
-		for( x = 0; x < widthr; x++) {
-			ir = x * y;
-			p = imr->imgdata.rawdata.raw_image[ir];
-			if (p > maxr) maxr = p;
-			else if (p < minr) minr = p;
-		}
-	}
-
-	printf("Image:\r\n");
-    for( y = 0; y < height; y++) {
-		for( x = 0; x < width; x++) {
-			i = x * y;
-			for( c= 0; c < 4; c++) {
-				p = imr->imgdata.image[i][c];
-				if (p > max) max = p;
-				else if (p < min) min = p;
-			}
-		}
-	}
-
-	printf("maxr: %d\r\nminr: %d\r\nmax: %d\r\nmin: %d", maxr, minr, max, min);*/
 }
 
 int main() {
