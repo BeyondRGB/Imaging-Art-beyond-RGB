ef5a2de9c1614ac916c8c241aa0d83e13897f573
tfk8939 <tfk8939@rit.edu>
2021-11-18 16:56:29 -0500

    refactored to use communication_obj for coms between front end and backend

---
+++ backend/src/server/comunication_obj.h
@@ -0,0 +1,34 @@
+//
+// Class for sending messages back to the sender of TCP connection
+//
+
+#ifndef COMMUNICATION_OBJ_H
+#define COMMUNICATION_OBJ_H
+
+#include <iostream>
+using namespace std;
+
+#define ASIO_STANDALONE
+#define _WEBSOCKETPP_CPP11_THREAD_
+#define _WEBSOCKETPP_CPP11_STRICT_ 
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+typedef server::message_ptr message_ptr;
+
+class CommunicationObj{
+private:
+	server* server_m = NULL;
+	websocketpp::connection_hdl connectionHandle_m;
+	websocketpp::frame::opcode::value opcode_m;
+
+public:
+	CommunicationObj() {};
+	CommunicationObj(server* s, websocketpp::connection_hdl hd1, message_ptr msg);
+	CommunicationObj(const CommunicationObj &other);
+	void send_msg(string msg);
+};
+
+#endif // COMMUNICATION_OBJ_H
