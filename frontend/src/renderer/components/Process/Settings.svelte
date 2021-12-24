afe497a4ad48ac0bf728b05cc80b172d5e74ce59
pxd5295 <pxd5295@rit.edu>
2021-12-23 19:14:06 -0500

    minor style changes

--- frontend/src/renderer/components/Process/Settings.svelte
+++ frontend/src/renderer/components/Process/Settings.svelte
@@ -4,15 +4,15 @@
   export let nextTab: any;
 
   import Sorter from "@components/Preprocess/Sorter.svelte";
-  import FileSelector from "@components/Preprocess/FileSelector.svelte";
+  // import FileSelector from "@components/Preprocess/FileSelector.svelte";
 
   $: console.log(files);
 </script>
 
 <main class="dark:text-white">
   <div class="settings">
-    <FileSelector title="Select Files:" />
-    <FileSelector title="Export Destination:" directory />
+    <!-- <FileSelector title="Select Files:" />
+    <FileSelector title="Export Destination:" directory /> -->
     <div>
       Specify File Roles:
       <Sorter />
@@ -27,7 +27,6 @@
   main {
     @apply w-full h-full px-[10%] pt-[5%] items-start justify-center;
   }
-
   .next {
     @apply mb-12;
   }
