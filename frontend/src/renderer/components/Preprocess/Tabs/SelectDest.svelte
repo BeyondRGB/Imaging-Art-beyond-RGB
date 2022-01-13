9e14af0b86785fc509b7f6f8629bddeb71114fa3
pxd5295 <pxd5295@rit.edu>
2022-01-13 17:18:31 -0500

    Added process tab, updating branch for thur meeting

---
+++ frontend/src/renderer/components/Preprocess/Tabs/SelectDest.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+</script>
+
+<main>
+  <left>
+    <h1>Select Destination Files Location</h1>
+    <p>
+      Select the folder where you would like to save the spectral images after
+      processing
+    </p>
+  </left>
+  <right> <input type="file" /> </right>
+</main>
+
+<style lang="postcss">
+  main {
+    @apply flex justify-between h-full w-full overflow-hidden;
+  }
+  left {
+    @apply bg-gray-600 w-full h-full p-6 flex-col;
+  }
+  right {
+    @apply bg-gray-700 w-full h-full p-6 flex justify-center;
+  }
+  h1 {
+    @apply text-3xl;
+  }
+  p {
+    @apply text-center pt-[30vh] bg-gray-500/25 m-6 h-[90%] rounded-lg;
+  }
+  input {
+    @apply translate-y-[35vh];
+  }
+</style>
--- frontend/src/renderer/pages/Preprocess.svelte
+++ frontend/src/renderer/pages/Preprocess.svelte
@@ -2,65 +2,89 @@
   import { currentPage } from "@util/stores";
   import Settings from "@components/Process/Settings.svelte";
   import ColorTarget from "@components/Process/ColorTarget.svelte";
+  import ImportImages from "@components/Preprocess/Tabs/ImportImages.svelte";
+  import SelectDest from "@components/Preprocess/Tabs/SelectDest.svelte";
+  import SpecFileRoles from "@components/Preprocess/Tabs/SpecFileRoles.svelte";
+  import AdvOpts from "@components/Preprocess/Tabs/AdvOpts.svelte";
   import { fade } from "svelte/transition";
-  let currentTab: any = Settings;
+  let currentTab: number = 0;
   let currName: string = "Settings";
 
-  function nextTab(current: string) {
-    if (current === "ColorTarget") {
-      // Set Page
+  let tabs: any = [
+    { name: "Import Images", component: ImportImages },
+    { name: "Select Destination", component: SelectDest },
+    { name: "Specifiy File Roles", component: SpecFileRoles },
+    { name: "Advanced Options", component: AdvOpts },
+  ];
+
+  function nextTab() {
+    if (currentTab !== tabs.length - 1) {
+      currentTab += 1;
     } else {
-      console.log("next tab");
-      currentTab = ColorTarget;
-      currName = "ColorTarget";
+      console.log("Error overflow");
     }
   }
-  function prevTab(current: string) {
-    if (current === "ColorTarget") {
-      currentTab = Settings;
-      currName = "Settings";
+
+  function prevTab() {
+    if (currentTab !== 0) {
+      currentTab -= 1;
     } else {
-      // Set Page
-      currentPage.set("Home");
+      console.log("Error overflow");
     }
   }
 </script>
 
 <main>
   <nav class="dark:bg-gray-800/25">
-    <button id="backBtn" on:click={() => prevTab(currName)}>Back</button>
+    <button id="backBtn" on:click={prevTab}>Back</button>
     <tabs>
-      <div class="tab" id="tab1" />
-      <div class="tab {currentTab === Settings ? 'none' : ''}" id="tab2" />
+      {#each tabs as tab}
+        <div
+          class="tab {tabs[currentTab].name !== tab.name ? 'none' : ''}"
+          id={tab.name}
+        />
+      {/each}
+      <!-- <div class="tab" id="tab1" />
+      <div class="tab {currentTab === 0 ? 'none' : ''}" id="tab2" /> -->
     </tabs>
   </nav>
-  {#key currentTab}
-    <div
-      class="page"
-      in:fade={{ duration: 200, delay: 200 }}
-      out:fade={{ duration: 200 }}
-    >
-      <svelte:component this={currentTab} {nextTab} />
-    </div>
-  {/key}
+  <div class="page">
+    {#key currentTab}
+      <div in:fade={{ duration: 200, delay: 200 }} out:fade={{ duration: 200 }}>
+        <svelte:component this={tabs[currentTab].component} />
+      </div>
+    {/key}
+  </div>
+  <botnav class="dark:bg-transparent">
+    <button on:click={nextTab} class="nextBtn">Next</button>
+  </botnav>
 </main>
 
 <style lang="postcss" local>
   main {
-    @apply relative h-full w-full overflow-hidden;
+    @apply relative h-full w-full overflow-hidden flex flex-col min-h-0;
   }
   nav {
     @apply flex h-12;
   }
+  botnav {
+    @apply flex h-10 justify-end;
+  }
   tabs {
     @apply flex-grow justify-center flex mr-[5vw];
+  }
+  .nextBtn {
+    @apply m-4;
   }
   .page {
     overflow: overlay;
     @apply w-full h-full;
   }
+  div {
+    @apply w-full h-full;
+  }
   .tab {
-    @apply w-16 h-2 rounded-full bg-green-400 self-center mx-2 ring-1 ring-gray-600;
+    @apply w-16 h-1 rounded-full bg-blue-400 self-center mx-2;
   }
   #backBtn {
     @apply absolute h-8 py-0 ml-2 my-2;
