e75b2eb83feb7d297a5563740e5a27ca4a490302
tfk8939 <tfk8939@rit.edu>
2022-01-15 12:55:45 -0500

    Set up Processing Manager to create and start specified processing thread

--- backend/src/server/process_manager.cpp
+++ backend/src/server/process_manager.cpp
@@ -9,14 +9,38 @@ using namespace jsoncons;
 
 void ProcessManager::process_request(std::string request, CommunicationObj coms_obj) {
 	std::cout << "Request: " << request << std::endl;
-	//TODO add requst parsing and logic for starting up spicific processing thread
-	std::string sample_request= R"({"RequestType":"processing from json","RequestData":{}})";
+	std::string sample_request= R"({
+		"RequestType":"processImg",
+		"RequestData":{}})";
+	//TODO pars the request once we are actualy sending json
 	jsoncons::json j = json::parse(sample_request);
-	std::string request_string = j.get(key_map[ProcessManager::RequestKey::REQUEST_TYPE]).as<std::string>();
+	std::string request_key = j.get(
+		key_map[ProcessManager::RequestKey::REQUEST_TYPE]).as<std::string>();
 
-#ifdef DEBUG
-	std::cout << "Debug flag is set" << std::endl;
-#endif
-	std::cout << "AAAAAAAAAAAAa" << std::endl;
+	std::shared_ptr<BackendProcess> process = identify_process(request_key, coms_obj);
+	std::cout << "CreatedProcess: " << process << std::endl;
+	std::thread p_thread(&ProcessManager::start_process, this, process);
+	p_thread.detach();
 
+}
+
+std::shared_ptr<BackendProcess> ProcessManager::identify_process(std::string key, CommunicationObj coms_obj) {
+	std::shared_ptr<BackendProcess> process(nullptr);
+	std::cout << "ProcessKey: " << key << std::endl;
+	if (key == "processImg") {
+		std::cout << "Creating Pipeline" << std::endl;
+		process = std::shared_ptr<Pipeline>(new Pipeline(coms_obj));
+	}
+	//TODO add other process instantiation here as more get created
+
+	return process;
+}
+
+void ProcessManager::start_process(std::shared_ptr<BackendProcess> process) {
+	if (nullptr == process) {
+		std::cout << "Failed to start process" << std::endl;
+	}
+	std::cout << "Starting Process:" << process << std::endl;
+	process->run();
+	std::cout << process->get_process_name() << " complete." << std::endl;
 }
