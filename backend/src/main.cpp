7c7e6d53376026c3f0206e3ecd91ab9004c055b6
tfk8939 <tfk8939@rit.edu>
2022-01-19 16:06:20 -0500

    cleaned up main

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -2,13 +2,25 @@
 #include <iostream>
 #include "server/request_server.hpp"
 
+void testFunc() {
+	// Add testing logc here if this is called the server will not be started
+
+}
+
 int main() {
+	bool test = false; // Set to true if you want to test something and bypass the server
+	if (test) {
+		testFunc();
+		std::cout << "Exit" << std::endl;
+		exit(1);
+	}
+
     std::cout << "BeyondRBG Backend Started: listening on port 9002\n";
     RequestServer server;
     server.init_server();
     server.start_server();
 
     //Block till server shuts down
-    std::cout << "Server Shutting Down.\n";
+    std::cout << "Exiting.\n";
     
 }
