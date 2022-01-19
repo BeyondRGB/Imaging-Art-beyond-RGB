0851bfce9536b3cd99d177130e13c668654b59ea
tfk8939 <tfk8939@rit.edu>
2022-01-19 12:02:26 -0500

    documentation

--- backend/src/utils/json.cpp
+++ backend/src/utils/json.cpp
@@ -14,75 +14,29 @@ Json::Json(jsoncons::json json_obj) {
 	this->json_obj = json_obj;
 }
 
-//T Json::get_value(std::string key, Type type) {
-//
-//	switch (type) {
-//	case Json::ARRAY:
-//		return this->json_obj.get(key).as<jsoncons::json::array>();
-//		break;
-//	case Json::BOOL:
-//		return this->json_obj.get(key).as<bool>();
-//		break;
-//	case Json::NUMBER:
-//		return this->json_obj.get(key).as<double>();
-//		break;
-//	case Json::STRING:
-//		return this->json_obj.get(key).as<std::string>();
-//		break;
-//	case Json::OBJECT:
-//		jsoncons::json obj = this->json_obj.get(key).as<jsoncons::json>();
-//		Json json(obj);
-//		return json;
-//		break;
-//	}
-//}
-
 bool Json::has(std::string key, Type type) {
 	bool contains = this->json_obj.contains(key);
 	bool is_type = false;
-	if (this->json_obj.contains(key)) {
+	if (contains) {
 		jsoncons::json item = this->json_obj.get(key);
 		Type item_type = this->get_type(item);
-		std::cout << "Type: " << item_type << std::endl;
 		if (item_type == type || type == Type::ANNY) {
 			is_type = true;
 		}
-		//switch (type)
-		//{
-		//case Json::ANNY:
-		//	is_type = true;
-		//	break;
-		//case Json::ARRAY:
-		//	is_type = item.is_object();
-		//	break;
-		//case Json::BOOL:
-		//	is_type = item.is_bool();
-		//	break;
-		////case Json::INT:
-		////	//is_type = item.is_i();
-		////	break;
-		//case Json::NUMBER:
-		//	is_type = item.is_number();
-		//	break;
-		//case Json::STRING:
-		//	is_type = item.is_string();
-		//	break;
-		//case Json::OBJECT:
-		//	is_type = item.is_object();
-		//	break;
-		//	return is_type;
-		//}
 	}
 	return is_type;
 }
 
 bool Json::get_bool(std::string key) {
-	if (!this->has(key, Type::BOOL))
+	validate_is_obj();
+	if (!this->has(key, Type::BOOL)) {
 		throw ParsingError("Bool('" + key + "') not found");
+	}
 	return this->json_obj.get(key).as<bool>();
 }
 
 bool Json::bool_at(int index) {
+	validate_is_array();
 	validate_bounds(index);
 	jsoncons::json item = this->json_obj[index];
 	if (!this->get_type(item) == Type::BOOL) {
@@ -92,12 +46,15 @@ bool Json::bool_at(int index) {
 }
 
 std::string Json::get_string(std::string key) {
-	if (!this->has(key, Type::STRING))
+	validate_is_obj();
+	if (!this->has(key, Type::STRING)) {
 		throw ParsingError("String('" + key + "') not found");
+	}
 	return this->json_obj.get(key).as<std::string>();
 }
 
 std::string Json::string_at(int index) {
+	validate_is_array();
 	validate_bounds(index);
 	jsoncons::json item = this->json_obj[index];
 	if (!this->get_type(item) == Type::STRING) {
@@ -107,12 +64,15 @@ std::string Json::string_at(int index) {
 }
 
 double Json::get_number(std::string key) {
-	if (!this->has(key, Type::NUMBER))
+	validate_is_obj();
+	if (!this->has(key, Type::NUMBER)) {
 		throw ParsingError("Number('" + key + "') not found");
+	}
 	return this->json_obj.get(key).as<double>();
 }
 
 double Json::number_at(int index) {
+	validate_is_array();
 	validate_bounds(index);
 	jsoncons::json item = this->json_obj[index];
 	if (!this->get_type(item) == Type::NUMBER) {
@@ -122,14 +82,17 @@ double Json::number_at(int index) {
 }
 
 Json Json::get_obj(std::string key) {
-	if (!this->has(key, Type::OBJECT))
+	validate_is_obj();
+	if (!this->has(key, Type::OBJECT)) {
 		throw ParsingError("Object('" + key + "') not found");
+	}
 	jsoncons::json obj = this->json_obj.get(key).as<jsoncons::json>();
 	Json json(obj);
 	return json;
 }
 
 Json Json::obj_at(int index) {
+	validate_is_array();
 	validate_bounds(index);
 	jsoncons::json item = this->json_obj[index];
 	if (!this->get_type(item) == Type::OBJECT) {
@@ -140,53 +103,47 @@ Json Json::obj_at(int index) {
 }
 
 Json Json::get_array(std::string key) {
-	if (!this->has(key, Type::ARRAY)) 
+	validate_is_obj();
+	if (!this->has(key, Type::ARRAY)) {
 		throw ParsingError("Array('" + key + "') not found");
+	}
 	jsoncons::json array = this->json_obj.get(key).as<jsoncons::json>();;
 	Json j(array);
 	return j;
 }
-//template <typename T>
-//JsonArray<T> Json::get_array(std::string key) {
-//	if (!this->has(key, Type::ARRAY))
-//		throw ParsingError("Array('" + key + "') not found");
-//	jsoncons::json array = this->json_obj.get(key);
-//	const auto item = array[0];
-//	std::cout << "Item: " << item << " Type: " << item.type() << std::endl;
-//	
-//	JsonArray<int> json_array(array);
-//	int itemat = json_array.at(1);
-//	std::cout << "ItemAt: " << itemat << std::endl;
-//	return json_array;
-//}
 
 int Json::get_size() {
 	return this->json_obj.size();
 }
 
 void Json::validate_bounds(int index) {
-	if (index >= this->get_size())
+	if (index >= this->get_size()) {
 		throw ParsingError("Index out of bounds");
+	}
+}
+
+void Json::validate_is_array() {
+	if (this->get_type() != Json::Type::ARRAY) {
+		throw ParsingError("Current Item is not a Json Array");
+	}
+}
+
+void Json::validate_is_obj() {
+	if (this->get_type() != Json::Type::OBJECT) {
+		throw ParsingError("Current Item is not a Json Object");
+	}
 }
 
 Json::Type Json::get_type(std::string key) {
-	if (!this->has(key, Type::ANNY))
+	if (!this->has(key, Type::ANNY)) {
 		throw ParsingError(key + " not found");
+	}
 	jsoncons::json item = this->json_obj[key];
 	return this->get_type(item);
-	//const jsoncons::json_type type = this->json_obj[key].type();
-	//std::cout << "Json type: " << type << std::endl;
-	//if (type == jsoncons::json_type::bool_value)
-	//	return Type::BOOL;
-	//if (type == jsoncons::json_type::string_value)
-	//	return Type::STRING;
-	//if (type == jsoncons::json_type::double_value)
-	//	return Type::NUMBER;
-	//if (type == jsoncons::json_type::array_value)
-	//	return Type::ARRAY;
-	//if (type == jsoncons::json_type::object_value)
-	//	return Type::OBJECT;
-	//return Type::NONE;
+}
+
+Json::Type Json::get_type() {
+	return get_type(this->json_obj);
 }
 
 Json::Type Json::get_type(jsoncons::json item) {
--- backend/src/utils/json.hpp
+++ backend/src/utils/json.hpp
@@ -1,9 +1,6 @@
 #ifndef JSON_H
 #define JSON_H
 #include <jsoncons/json.hpp>
-#include "json_array.hpp"
-
-
 
 class Json {
 
@@ -21,33 +18,187 @@ public:
 
 	Json(std::string json_str);
 
-
+	/**
+	* Check to see if json contains item of given type
+	* @param key: the key to the item to be checked for
+	* @param type: the Json::Type to check for
+	*	if none given this will default to ANNY and 
+	*	will only check that the key exists
+	* @return: true if key exists and is of given type
+	*	false if key does not exist or does not match type
+	*/
 	bool has(std::string key, Type type = Type::ANNY);
 	
+	/**
+	* Extract boolean value from Json
+	* This assumes that the json item this parser currently holds
+	* is a Json Object
+	*	thows ParsingError if:
+			not holding a Json obj or
+	*		key of type bool is not found
+	* @param key: the key to get value for
+	* @return: the boolean value for given key
+	*/
 	bool get_bool(std::string key);
+
+	/**
+	* Extract string value from Json
+	* This assumes that the json item this parser currently holds
+	* is a Json Object
+	*	thows ParsingError if:
+			not holding a Json obj or
+	*		key of type string is not found
+	* @param key: the key to get value for
+	* @return: the string value for given key
+	*/
 	std::string get_string(std::string key);
+
+	/**
+	* Extract string value from Json
+	* This assumes that the json item this parser currently holds
+	* is a Json Object
+	*	thows ParsingError if:
+			not holding a Json obj or
+	*		key of type obj is not found
+	* @param key: the key to get value for
+	* @return: a new instance of this class containing the json object
+	*/
 	Json get_obj(std::string key);
+
+	/**
+	* Extract numeric value from Json
+	* This assumes that the json item this parser currently holds
+	* is a Json Object
+	*	thows ParsingError if:
+			not holding a Json obj or
+	*		key with numeric value is not found
+	* @param key: the key to get value for
+	* @return: the numeric value for given key
+	*/
 	double get_number(std::string key);
-	//template <typename T>
-	//JsonArray<T> get_array(std::string key);
+	
+	/**
+	* Extract string value from Json
+	* This assumes that the json item this parser currently holds
+	* is a Json Object
+	*	thows ParsingError if:
+			not holding a Json obj or
+	*		key of type array is not found
+	* @param key: the key to get value for
+	* @return: a new instance of this class containing the json array
+	*/
 	Json get_array(std::string key);
 
+	/**
+	* Get numeric value at index from Json Array
+	* This assumes that the json item this parser currently holds
+	* is a Json Array
+	*	thows ParsingError if:
+			not holding a Json Array or
+			index out of bounds
+	*		item at index is not numeric
+	* @param index: integer index
+	* @return: the numeric value for given index
+	*/
 	double number_at(int index);
+
+	/**
+	* Get string value at index from Json Array
+	* This assumes that the json item this parser currently holds
+	* is a Json Array
+	*	thows ParsingError if:
+			not holding a Json Array or
+			index out of bounds
+	*		item at index is not a string
+	* @param index: integer index
+	* @return: the string value for given index
+	*/
 	std::string string_at(int index);
+
+	/**
+	* Get json object at index from Json Array
+	* This assumes that the json item this parser currently holds
+	* is a Json Array
+	*	thows ParsingError if:
+			not holding a Json Array or
+			index out of bounds
+	*		item at index is not numeric
+	* @param index: integer index
+	* @return: a new instance of this class containing the json object
+	*/
 	Json obj_at(int index);
+
+	/**
+	* Get boolean value at index from Json Array
+	* This assumes that the json item this parser currently holds
+	* is a Json Array
+	*	thows ParsingError if:
+			not holding a Json Array or
+			index out of bounds
+	*		item at index is not numeric
+	* @param index: integer index
+	* @return: bool
+	*/
 	bool bool_at(int index);
 
+	/**
+	* Converts the current json item to a string
+	* @param should_beutify: (optional) default to false
+	*	if true add newlines and tabs to the string
+	* @return: json string
+	*/
 	std::string to_string(bool should_beutify = false);
 
+	/**
+	* The size of the json obj or array held by this class
+	* @return: int representing the size
+	*/
 	int get_size();
+
+	/**
+	* Get the type of the value found with given key
+	* This assumes the json item this class holds is a Json Object
+	*	throws ParsingError if:
+	*		key not presint in json object
+	* @param key: the key for the item in question
+	* @return: the Json::Type of the value found at key
+	*/
 	Json::Type get_type(std::string key);
+
+	/**
+	* Gets the type of the json item this class currently holds
+	* @return: the Json::Type(OBJECT or ARRAY)
+	*/
+	Json::Type get_type();
 
 
 private:
 	Json(jsoncons::json json_obj);
 	jsoncons::json json_obj;
 
+	/**
+	* Get the type of the given item
+	* @param item: the item to get the type for
+	* @return: Json::Type
+	*/
 	Json::Type get_type(jsoncons::json item);
+
+	/**
+	* Validates that the current json item held is a Json Array
+	* thows ParsingError if not
+	*/
+	void validate_is_array();
+
+	/**
+	* Validates that the current json item held is a Json Object
+	* throws ParsingError if not
+	*/
+	void validate_is_obj();
+
+	/**
+	* Validates that the given index is within the bounds of the current Json Array
+	* throws ParsingError if not
+	*/
 	void validate_bounds(int index);
 
 };
@@ -62,16 +213,4 @@ public:
 private:
 	std::string error = "Undefined Json Parsing Error";
 };
-
-//template <typename T>
-//JsonArray<T> Json::get_array(std::string key) {
-//	if (!this->has(key, Type::ARRAY))
-//		throw ParsingError("Array('" + key + "') not found");
-//	jsoncons::json array = this->json_obj.get(key);
-//	const auto item = array[0];
-//	std::cout << "Item: " << item << " Type: " << item.type() << std::endl;
-//
-//	JsonArray<T> json_array(array);
-//	return json_array;
-//}
 #endif // !JSON_H
