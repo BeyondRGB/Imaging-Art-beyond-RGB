81bfa6d6fcb7b2e96aea5d9c24c2d31bc23379b2
tfk8939 <tfk8939@rit.edu>
2022-01-21 14:23:23 -0500

    Can read in and print out lines from csv file

--- backend/src/reference_data/data_manager.cpp
+++ backend/src/reference_data/data_manager.cpp
@@ -2,13 +2,6 @@
 
 DataManager* DataManager::instance = nullptr;
 
-//static DataManager* DataManager::get_instance() {
-//	if (nullptr == instance) {
-//		instance = new DataManager();
-//	}
-//	return instance;
-//}
-
 RefData* DataManager::get_ref_data(std::string key) {
 	if (!this->data_map.contains(key)) {
 		//For now key is filename
--- backend/src/reference_data/ref_data.cpp
+++ backend/src/reference_data/ref_data.cpp
@@ -1,4 +1,6 @@
 #include "ref_data.hpp"
+#include <iostream>
+#include <fstream>
 
 RefData::RefData(std::string file_path) {
 	this->read_in_data(file_path);
@@ -6,4 +8,10 @@ RefData::RefData(std::string file_path) {
 
 void RefData::read_in_data(std::string file_path) {
 	std::cout << "Reading File: " << file_path << std::endl;
+	std::ifstream ref_file(file_path);
+	std::string line;
+	while (std::getline(ref_file, line)) {
+		std::cout << line << std::endl;
+	}
+	ref_file.close();
 }
