eba2c6bde50bd34d4081b5503dd890c421e235f8
tfk8939 <tfk8939@rit.edu>
2022-01-23 08:53:45 -0500

    Added StandardObservers to the DataManager

--- backend/src/reference_data/data_manager.hpp
+++ backend/src/reference_data/data_manager.hpp
@@ -2,7 +2,9 @@
 #define DATA_MANAGER_H
 
 #include "ref_data.hpp"
+#include "standard_observer.hpp"
 #include <unordered_map>
+
 class DataManager {
 
 public:
@@ -12,14 +14,22 @@ public:
 		}
 		return instance;
 	}
+
+	void shut_down();
+
 	RefData* get_ref_data(std::string key);
+	StandardObserver* get_observer_1931();
+	StandardObserver* get_observer_1964();
 
 private:
 	DataManager() {};
-	~DataManager() { delete instance; };
+	~DataManager();
 
 	static DataManager* instance;
+	StandardObserver* so1931 = nullptr;
+	StandardObserver* so1964 = nullptr;
 	std::unordered_map<std::string, RefData*> data_map;
+
 
 };
 
