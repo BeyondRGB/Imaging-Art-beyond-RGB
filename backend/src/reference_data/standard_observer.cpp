8fd4b648e8bbcd49e2d81d75dbdf9d1f2b95fff2
tfk8939 <tfk8939@rit.edu>
2022-01-23 14:44:05 -0500

    Reworked ColorPatch to use RefDataArray

--- backend/src/reference_data/standard_observer.cpp
+++ backend/src/reference_data/standard_observer.cpp
@@ -18,9 +18,9 @@ StandardObserver::~StandardObserver() {
 }
 
 void StandardObserver::init(std::string file) {
-	this->x = new double[STANDARD_OBSERVER_SIZE];
-	this->y = new double[STANDARD_OBSERVER_SIZE];
-	this->z = new double[STANDARD_OBSERVER_SIZE];
+	this->x = new RefDataArray(STANDARD_OBSERVER_SIZE);
+	this->y = new RefDataArray(STANDARD_OBSERVER_SIZE);
+	this->z = new RefDataArray(STANDARD_OBSERVER_SIZE);
 	std::cout << "Generateing StandardObserver" << std::endl;
 	this->open_file(file);
 	// Ignore header
@@ -29,62 +29,79 @@ void StandardObserver::init(std::string file) {
 		std::string line = this->get_next_line();
 		// Ignor wavelength
 		this->get_next<double>(line);
-		this->x[i] = this->get_next<double>(line);
-		this->y[i] = this->get_next<double>(line);
-		this->z[i] = this->get_next<double>(line);
+		this->x->append(this->get_next<double>(line));
+		this->y->append(this->get_next<double>(line));
+		this->z->append(this->get_next<double>(line));
 	}
 	this->close_file();
 }
 
-double StandardObserver::x_by_index(int index) {
-	if (index < REFLECTANCE_SIZE)
-		return this->x[index];
-	else
-		throw std::out_of_range("Index out of bounds");
-}
-
-double StandardObserver::y_by_index(int index) {
-	if (index < REFLECTANCE_SIZE)
-		return this->y[index];
-	else
-		throw std::out_of_range("Index out of bounds");
-}
-
-double StandardObserver::z_by_index(int index) {
-	if (index < REFLECTANCE_SIZE)
-		return this->z[index];
-	else
-		throw std::out_of_range("Index out of bounds");
-}
-
-double StandardObserver::x_by_wavelen(int wavelen) {
-	return this->x_by_index(WAVELEN_TO_INDEX(wavelen));
-}
-
-double StandardObserver::y_by_wavelen(int wavelen) {
-	return this->y_by_index(WAVELEN_TO_INDEX(wavelen));
-}
-
-double StandardObserver::z_by_wavelen(int wavelen) {
-	return this->z_by_index(WAVELEN_TO_INDEX(wavelen));
-}
-
-double StandardObserver::sum_y() {
-	return this->sum(this->y);
-}
-
-double StandardObserver::sum_x() {
-	return this->sum(this->x);
-}
-
-double StandardObserver::sum_z() {
-	return this->sum(this->z);
-}
-
-double StandardObserver::sum(double* array) {
-	double sum = 0;
-	for (int i = 0; i < STANDARD_OBSERVER_SIZE; i++) {
-		sum += array[i];
+double StandardObserver::value_by_index(ValueType type, int index) {
+	switch (type) {
+	case X:
+		return this->x->get_by_index(index);
+	case Y:
+		return this->y->get_by_index(index);
+	case Z:
+		return this->z->get_by_index(index);
+	default:
+		return this->y->get_by_index(index);
 	}
-	return sum;
 }
+
+double StandardObserver::value_by_wavelen(ValueType type, int wavelen) {
+	return this->value_by_index(type, WAVELEN_TO_INDEX(wavelen));
+}
+
+//double StandardObserver::x_by_index(int index) {
+//	if (index < REFLECTANCE_SIZE)
+//		return this->x[index];
+//	else
+//		throw std::out_of_range("Index out of bounds");
+//}
+//
+//double StandardObserver::y_by_index(int index) {
+//	if (index < REFLECTANCE_SIZE)
+//		return this->y[index];
+//	else
+//		throw std::out_of_range("Index out of bounds");
+//}
+//
+//double StandardObserver::z_by_index(int index) {
+//	if (index < REFLECTANCE_SIZE)
+//		return this->z[index];
+//	else
+//		throw std::out_of_range("Index out of bounds");
+//}
+//
+//double StandardObserver::x_by_wavelen(int wavelen) {
+//	return this->x_by_index(WAVELEN_TO_INDEX(wavelen));
+//}
+//
+//double StandardObserver::y_by_wavelen(int wavelen) {
+//	return this->y_by_index(WAVELEN_TO_INDEX(wavelen));
+//}
+//
+//double StandardObserver::z_by_wavelen(int wavelen) {
+//	return this->z_by_index(WAVELEN_TO_INDEX(wavelen));
+//}
+//
+//double StandardObserver::sum_y() {
+//	return this->sum(this->y);
+//}
+//
+//double StandardObserver::sum_x() {
+//	return this->sum(this->x);
+//}
+//
+//double StandardObserver::sum_z() {
+//	return this->sum(this->z);
+//}
+//
+//double StandardObserver::sum(double* array) {
+//	double sum = 0;
+//	for (int i = 0; i < STANDARD_OBSERVER_SIZE; i++) {
+//		sum += array[i];
+//	}
+//	return sum;
+//}
--- backend/src/reference_data/standard_observer.hpp
+++ backend/src/reference_data/standard_observer.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "csv_parser.hpp"
 #include "ref_data_defines.hpp"
+#include "ref_data_array.hpp"
 
 class StandardObserver : public CSVParser {
 
@@ -13,24 +14,29 @@ public:
 		SO_1931,
 		SO_1964
 	};
+	enum ValueType {
+		X,Y,Z
+	};
 	StandardObserver(ObserverType type);
 	~StandardObserver();
 	void init(std::string file);
-	double x_by_index(int index);
-	double y_by_index(int index);
-	double z_by_index(int index);
-	double x_by_wavelen(int wavelen);
-	double y_by_wavelen(int wavelen);
-	double z_by_wavelen(int wavelen);
-	double sum_y();
-	double sum_x();
-	double sum_z();
+	double value_by_index(ValueType type, int index);
+	double value_by_wavelen(ValueType type, int wavelen);
+	//double x_by_index(int index);
+	//double y_by_index(int index);
+	//double z_by_index(int index);
+	//double x_by_wavelen(int wavelen);
+	//double y_by_wavelen(int wavelen);
+	//double z_by_wavelen(int wavelen);
+	//double sum_y();
+	//double sum_x();
+	//double sum_z();
 
 private:
-	double* x = nullptr;
-	double* y = nullptr;
-	double* z = nullptr;
-	double sum(double* array);
+	RefDataArray* x = nullptr;
+	RefDataArray* y = nullptr;
+	RefDataArray* z = nullptr;
+	//double sum(double* array);
 };
 
 #endif // !STANDARD_OBSERVER_H
