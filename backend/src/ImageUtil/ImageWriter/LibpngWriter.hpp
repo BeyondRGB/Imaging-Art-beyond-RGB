a9f0c7e8037420c117c70dfd45765f94c256d005
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-01-25 15:34:23 -0500

    Successful PNG writing
    Reduced time of writing the full image size of the Nikon test raws to around two seconds. Rough conversion to 8 bits to save time & space as well as reduced compression quality to save time.
    
    Temporary changes to pipeline for testing.

--- backend/src/ImageUtil/ImageWriter/LibpngWriter.hpp
+++ backend/src/ImageUtil/ImageWriter/LibpngWriter.hpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <stdio.h>
 
-#include <png.h>
 #include <zlib.h>
+#include <png.h>
 
 #include "ImageUtil/Image.hpp"
 #include "ImageWriter.hpp"
@@ -19,12 +19,16 @@ namespace btrgb {
             ~LibpngWriter();
         protected:
             void _write(image* im, std::string filename) override;
-
     };
 
     class Libpng_OpenFileFailed : public ImageWritingError {
         public:
         virtual char const * what() const noexcept { return "Libpng Error: Cannot open file for writing."; }
+    };
+
+    class Libpng_LibraryInitFailed : public ImageWritingError {
+        public:
+        virtual char const * what() const noexcept { return "Libpng Error: Library initialization failed."; }
     };
 
     class Libpng_WriteFailed : public ImageWritingError {
