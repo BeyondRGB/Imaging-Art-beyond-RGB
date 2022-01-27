5840c6a53d6443926f67578ccbc991635ec6e470
tfk8939 <tfk8939@rit.edu>
2022-01-27 11:29:06 -0500

    Added doc string to CSVParser

--- backend/src/reference_data/csv_parser.hpp
+++ backend/src/reference_data/csv_parser.hpp
@@ -6,16 +6,68 @@
 #include <fstream>
 #include <boost/lexical_cast.hpp>
 
+/**
+* Class for parsing csv files
+*/
 class CSVParser {
 
 public:
 	CSVParser() {}
+
+	/**
+	* Open given file. The file steam is stored as a private 
+	* member variable.
+	* It is the responsibility of the caller of open to 
+	* call close_file() when done
+	* @param file: the file name and path to be opend
+	* @return: true if opend succsessfuly 
+	*		   false if not
+	*/
 	bool open_file(std::string file);
+
+	/**
+	* Close the file
+	*/
 	void close_file();
+
+	/**
+	* Counts the number of tokens in the given line
+	* @param line: string to count tokens in
+	* @param delimiter: string used to seperate tokens in line. 
+	*	This defaults to ","
+	* @return: count of tokens seperated by delimiter
+	*/
 	int count_line_items(std::string line, std::string delimiter = ",");
+	
+	/**
+	* Identifies if the given line contains another token
+	* @param line: string to check for additional tokens
+	* @param delimiter: string used to seperate tokens in line
+	* @return: true if more tokens exist
+	*		   false if none exist
+	*/
 	bool has_next(std::string line, std::string delimiter = ",");
+	
+	/**
+	* Gets the next line from open file
+	* @return: line as a string
+	*/
 	std::string get_next_line();
+
+	/**
+	* Identifies if open file has another line to read
+	* @return: true if there exists another line
+	*		   false if not
+	*/
 	bool has_next_line();
+
+	/**
+	* Generic function for retriveing the next token from the given line
+	* This modifies the given line, removing the token returned
+	* @param line: the string to get token from
+	* @param delimiter: the string seperating tokens in line
+	* @return: token as type T
+	*/
 	template <typename T>
 	T get_next(std::string &line, std::string delimiter = ",") {
 		std::string token;
@@ -31,6 +83,9 @@ public:
 	}
 
 private:
+	/**
+	* Identifies if there is only token in the given line
+	*/
 	bool has_one_item(std::string line, std::string delimiter = ",");
 	std::ifstream file_m;
 
