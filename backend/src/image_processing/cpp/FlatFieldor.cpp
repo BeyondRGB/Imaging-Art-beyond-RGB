d410fa4908071846a4d7c470bc5895c20742df8a
Jake O'Neil <jgoneil03@gmail.com>
2022-01-29 14:44:33 -0500

    Adding \n to testing prints

--- backend/src/image_processing/cpp/FlatFieldor.cpp
+++ backend/src/image_processing/cpp/FlatFieldor.cpp
@@ -66,12 +66,12 @@ void FlatFieldor::execute(CallBackFunction func, btrgb::ArtObject* images) {
     int wWidth = tarWidth * (targetCols - whiteCol);
 
     //Testing
-    std::cout<<("*****************************");
-    std::cout<<("Top " + topEdge.to_string());
-    std::cout<<("Bot " + botEdge.to_string());
-    std::cout<<("Left " + leftEdge.to_string());
-    std::cout<<("Right " + rightEdge.to_string());
-    std::cout<<("*****************************");
+    std::cout<<("*****************************")<<std::endl;
+    std::cout<<("Top " + topEdge.to_string())<<std::endl;
+    std::cout<<("Bot " + botEdge.to_string())<<std::endl;
+    std::cout<<("Left " + leftEdge.to_string())<<std::endl;
+    std::cout<<("Right " + rightEdge.to_string())<<std::endl;
+    std::cout<<("*****************************")<<std::endl;
 
     int patchX = leftEdge + wWidth;
     int patchY = topEdge + wHeight;
