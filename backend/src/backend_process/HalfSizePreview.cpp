50284c2c21d665c14d647c5b79753659d3d216d4
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-01-28 23:29:17 -0500

    Re-ordered batch request to give individual responses

--- backend/src/backend_process/HalfSizePreview.cpp
+++ backend/src/backend_process/HalfSizePreview.cpp
@@ -3,37 +3,100 @@
 unsigned int HalfSizePreview::id = 0;
 
 HalfSizePreview::HalfSizePreview() {
-    this->set_process_name("GetHalfSizePreview(" + std::to_string(this->id++) + ")");
+    this->set_process_name("GetHalfSizePreview(" + std::to_string(HalfSizePreview::id) + ")");
+    HalfSizePreview::id += 1;
 }
 
 HalfSizePreview::~HalfSizePreview() {}
 
 
 void HalfSizePreview::run() {
+    std::cout << "started process" << std::endl;
+
     Json filenames = this->process_data_m->get_array("filenames");
-    LibRawReader reader(LibRawReader::LOAD_HALF_SIZE);
-    ManualBitDepthFinder bitDepthFinder;
-    LibpngWriter pngWriter;
+    const int ticket = 2345234;
+
+    LibRawReader* reader = new LibRawReader(LibRawReader::LOAD_HALF_SIZE);
+    ManualBitDepthFinder* bitDepthFinder = new ManualBitDepthFinder();;
+    btrgb::LibpngWriter* pngWriter = new btrgb::LibpngWriter();
+
     int raw_bit_depth;
+    std::string* rsp = nullptr;
+    btrgb::image* im = nullptr;
+    std::vector<uint8_t>* png_binary;
 
     
     for (int i = 0; i < filenames.get_size(); i++) {
 
-        btrgb::image im(filenames.string_at(i));
-        reader.read(&im);
+        try {
+            std::cout << "about to read raw..." << std::endl;
+            im = new btrgb::image(filenames.string_at(i));
+            reader->read(im);
 
-        raw_bit_depth = bitDepthFinder.get_bit_depth(&im);
+            std::cout << "read raw" << std::endl;
+            raw_bit_depth = bitDepthFinder->get_bit_depth(im);
+            std::cout << "got bit depth: " << std::to_string(raw_bit_depth) << std::endl;
 
-        std::vector<uint8_t> png_binary;
-        pngWriter.write_png(&im, "", &png_binary, raw_bit_depth);
-        
-        /* To do: 
-            - convert to base64
-            - format response message
-        */
+            png_binary = new std::vector<uint8_t>;
+            pngWriter->write_png(im, "", png_binary, raw_bit_depth);
+            std::cout << "wrote png" << std::endl;
+            
+            rsp = new std::string;
+            rsp->reserve(im->getTotalByteSize() * 2);
+            std::cout << "reserved size: " << std::to_string(im->getTotalByteSize() * 2) << std::endl;
+
+            rsp->append(R"({"RequestID":)");
+            rsp->append(std::to_string(ticket));
+            rsp->append(R"(,"RequestType":"HalfSizePreview","RequestData":{"filename":")");
+            rsp->append(im->filename());
+            rsp->append(R"(","dataURL": "data:image/png;base64,)");
+            rsp->append(cppcodec::base64_rfc4648::encode((const uint8_t*) &(*png_binary)[0], png_binary->size()));
+            rsp->append(R"("}})");
+
+            std::cout << "base64 and json complete" << std::endl;
+
+            this->send_msg(*rsp);
+
+
+        }
+        catch(const RawReaderStrategy_FailedToOpenFile& e) {
+            this->send_msg(R"({"RequestID":)");
+            rsp->append(std::to_string(ticket));
+            rsp->append(R"(,"RequestType":"HalfSizePreview","RequestData":{"filename":")");
+            rsp->append(im->filename());
+            rsp->append(R"(","dataURL": 0)");
+            rsp->append(R"(}})");
+        }
+        catch(...) {
+            this->send_msg(R"({"RequestID":)");
+            rsp->append(std::to_string(ticket));
+            rsp->append(R"(,"RequestType":"HalfSizePreview","RequestData":{"filename":")");
+            rsp->append(im->filename());
+            rsp->append(R"(","dataURL": 0)");
+            rsp->append(R"(}})");
+        }
+
+        if(rsp != nullptr) {
+            delete rsp;
+            rsp = nullptr;
+        }
+        if(png_binary != nullptr) {
+            delete png_binary;
+            png_binary = nullptr;
+        }
+        if(im != nullptr) {
+            delete im;
+            im = nullptr;
+        }
 
     }
 
+    delete reader;
+    delete bitDepthFinder;
+    delete pngWriter;
+
 }
 
+
+    
 
--- backend/src/backend_process/HalfSizePreview.hpp
+++ backend/src/backend_process/HalfSizePreview.hpp
@@ -3,14 +3,16 @@
 
 #include <vector>
 #include <stdint.h>
+#include <cppcodec/base64_rfc4648.hpp>
 
 #include "ImageUtil/Image.hpp"
+#include "ImageUtil/ImageWriter/LibpngWriter.hpp"
 #include "utils/json.hpp"
 #include "server/comunication_obj.hpp"
 #include "backend_process.hpp"
 #include "image_processing/header/LibRawReader.h"
 #include "image_processing/header/ManualBitDepthFinder.h"
-#include "ImageUtil/ImageWriter/LibpngWriter.hpp"
+
 
 
 class HalfSizePreview : public BackendProcess {
@@ -22,14 +24,6 @@ public:
 
 private:
 	static unsigned int id;
-    typedef std::vector<btrgb::image*> image_vector;
-    struct Png {
-        std::string filename;
-        std::vector<uint8_t> binary;
-    };
-    typedef std::vector<Png> png_image_vector;
-
-    //void send
 
 };
 
