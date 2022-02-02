5a8a3b2b2bc8e93c3eca2a2c266cc55c17ac4a67
tfk8939 <tfk8939@rit.edu>
2022-02-02 16:13:17 -0500

    Added a few comments

--- backend/linux_release.sh
+++ backend/linux_release.sh
@@ -6,9 +6,9 @@ else
 	echo The stuff
 	mkdir -p ./build/Release/resouces/app/backend/res/
 fi
-# Copy all backend resource files relative to where they will live in deployied app
+# Copy all backend resource files relative to where they will live in deploed app
 cp -r ./res/* ./build/Release/resouces/app/backend/res/
 
 # Build
-cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-linuxgit 
+cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-linux
 cmake --build ./build/ --config Release
