88c92fbe12579ac6933686343d695088cbe6e75f
tfk8939 <tfk8939@rit.edu>
2022-02-02 16:10:22 -0500

    Set up resource file deployment

---
+++ backend/linux_release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+if [ -d ./build/Release/resouces/app/backend/res/ ]
+then
+	echo Found ./build/Release/resouces/app/backend/res/
+else
+	echo The stuff
+	mkdir -p ./build/Release/resouces/app/backend/res/
+fi
+# Copy all backend resource files relative to where they will live in deployied app
+cp -r ./res/* ./build/Release/resouces/app/backend/res/
+
+# Build
+cmake -B build -S . -DVCPKG_TARGET_TRIPLET=x64-linuxgit 
+cmake --build ./build/ --config Release
