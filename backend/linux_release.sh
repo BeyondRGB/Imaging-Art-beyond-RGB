fa6ddd2463dca548ff6c30691b18e3b14af16fd6
tfk8939 <tfk8939@rit.edu>
2022-02-03 13:18:09 -0500

    cleaning up .sh conditions

--- backend/linux_release.sh
+++ backend/linux_release.sh
@@ -3,7 +3,6 @@ if [ -d ./build/Release/resouces/app/backend/res/ ]
 then
 	echo Found ./build/Release/resouces/app/backend/res/
 else
-	echo The stuff
 	mkdir -p ./build/Release/resouces/app/backend/res/
 fi
 # Copy all backend resource files relative to where they will live in deploed app
