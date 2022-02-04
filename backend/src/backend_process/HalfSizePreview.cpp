9412863d9dec368f77a20bbd0c14eef993e3a59d
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-02-04 10:05:44 -0500

    Fixed HalfSizePreview for float image

--- backend/src/backend_process/HalfSizePreview.cpp
+++ backend/src/backend_process/HalfSizePreview.cpp
@@ -18,7 +18,8 @@ void HalfSizePreview::run() {
     Json filenames = this->process_data_m->get_array("filenames");
     const int ticket = 2345234;
 
-    btrgb::HalfSizeReader reader;
+    btrgb::HalfSizeReader* reader = new btrgb::HalfSizeReader;
+    std::vector<uchar>* png_binary = nullptr;
     std::string* rsp = nullptr;
 
     
@@ -35,34 +36,35 @@ void HalfSizePreview::run() {
         try {
 
             std::cout << "about to read raw..." << std::endl;
-            reader.open(filenames.string_at(i));
-            cv::Mat im(reader.width(), reader.height(), CV_8UC(reader.channels()));
-            reader.copyBitmap( (uint8_t*) im.data, im.rows * im.cols * im.channels());
-            reader.release();
+            reader->open(filenames.string_at(i));
+            cv::Mat im(reader->height(), reader->width(), CV_8UC(reader->channels()));
+            reader->copyBitmap( (uint8_t*) im.data, im.rows * im.cols * im.channels());
+            reader->release();
             std::cout << "read raw" << std::endl;
 
             std::cout << "scaling preview..." << std::endl;
             cv::Mat im_scaled;
             double scaler = double(2000) / double(im.cols);
+            std::cout << "scaling by: " << scaler << std::endl;
             cv::resize(im, im_scaled, cv::Size(), scaler, scaler, cv::INTER_AREA);
             im.release();
             std::cout << "scaling done" << std::endl;
 
             std::cout << "writing png..." << std::endl;
-            std::vector<uchar> png_binary;
-            cv::imencode(".png", im_scaled, png_binary, png_params);
+            png_binary = new std::vector<uchar>;
+            cv::imencode(".png", im_scaled, *png_binary, png_params);
             im_scaled.release();
             std::cout << "wrote png" << std::endl;
             
             std::cout << "writing base64 and response..." << std::endl;
             rsp = new std::string;
-            rsp->reserve( png_binary.size() / 2 * 3 );
+            rsp->reserve( png_binary->size() / 2 );
             rsp->append(R"({"RequestID":)");
             rsp->append(std::to_string(ticket));
             rsp->append(R"(,"RequestType":"HalfSizePreview","RequestData":{"filename":")");
             rsp->append(std::regex_replace(filenames.string_at(i), std::regex("\\\\"), "\\\\"));
             rsp->append(R"(","dataURL": "data:image/png;base64,)");
-            rsp->append(cppcodec::base64_rfc4648::encode(png_binary));
+            rsp->append(cppcodec::base64_rfc4648::encode(*png_binary));
             rsp->append(R"("}})");
 
             std::cout << "base64 and json complete" << std::endl;
@@ -88,8 +90,14 @@ void HalfSizePreview::run() {
             delete rsp;
             rsp = nullptr;
         }
+        if(png_binary != nullptr) {
+            delete png_binary;
+            png_binary = nullptr;
+        }
 
     }
+
+    delete reader;
 
 }
 
