a09ccf6f250697a3f8caff20c513f9b52224d0af
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-02-07 13:16:18 -0500

    Removed unnecessary strategy pattern

--- backend/src/image_processing/header/BitDepthScaler.h
+++ backend/src/image_processing/header/BitDepthScaler.h
@@ -3,17 +3,13 @@
 
 
 #include "ImgProcessingComponent.h"
-#include "image_processing/header/BitDepthScalerStrategy.h"
 
 class BitDepthScaler : public ImgProcessingComponent {
 
     public:
-        BitDepthScaler(std::string strategy = "OpenCV Matrix forEach");
+        BitDepthScaler();
         ~BitDepthScaler();
         void execute(CallBackFunction func, btrgb::ArtObject* images) override;
-
-    private:
-        BitDepthScalerStrategy* strategy;
 };
 
 
