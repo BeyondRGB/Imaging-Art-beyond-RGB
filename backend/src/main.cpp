16ac81dbbe397c8702bffb61459bfd0fc3e6eafa
tfk8939 <tfk8939@rit.edu>
2022-02-11 12:46:53 -0500

    Cleaning up main

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,29 +1,17 @@
 
 #include <iostream>
 #include "server/request_server.hpp"
-#include "spdlog/spdlog.h"
 #include "utils/logger.hpp"
-
-
-void testOther() {
-    LOG_ERROR("FromOther");
-    LOG_INFO("Info from test Other");
-}
 
 //Testing Includes: Remove before submiting PR
 void testFunc() {
     
-    LOG_ERROR("Test 1 2 3");
-    LOG_INFO("From testFunc");
-    LOG_ERROR("Test 456");
-    testOther();
-    LOG_ERROR("Test 789");
 }
 
 
 int main() {
     Logger::init_logging();
-	bool test = true; // Set to true if you want to test something and bypass the server
+	bool test = false; // Set to true if you want to test something and bypass the server
 	if (test) {
 		testFunc();
 		std::cout << "Exit" << std::endl;
