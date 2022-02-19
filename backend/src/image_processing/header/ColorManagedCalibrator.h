e66ad0d86f4ccec3060ef754de7c98646e1ab68d
tfk8939 <tfk8939@rit.edu>
2022-02-19 10:07:55 -0500

    Initial set up for calculating deltaE

--- backend/src/image_processing/header/ColorManagedCalibrator.h
+++ backend/src/image_processing/header/ColorManagedCalibrator.h
@@ -8,6 +8,7 @@
 #include "utils/csv_parser.hpp"
 #include "ImgProcessingComponent.h"
 #include "reference_data/ref_data_defines.hpp"
+#include "reference_data/ref_data.hpp"
 
 class ColorManagedCalibrator : public ImgProcessingComponent{
 public:
@@ -17,17 +18,21 @@ public:
 
 
     void build_input_matrix();
-    void display_avg_matrix(cv::Mat* matrix);
+    void display_matrix(cv::Mat* matrix);
 
 private:
-    cv::Mat* color_patch_avgs = nullptr;
+    cv::Mat color_patch_avgs;
     cv::Mat optimization_input;
     cv::Mat M;
     cv::Mat offest;
+    RefData* ref_data;
+
 
 
     ColorTarget get_target(btrgb::ArtObject* images, btrgb::Image* im);
     void build_target_avg_matrix(ColorTarget targets[], int target_count, int channel_count);
+    void find_optimization();
+    double compute_deltaE(cv::Mat);
     
     //void build_input_matrix();
     //void display_avg_matrix(cv::Mat* matrix);
