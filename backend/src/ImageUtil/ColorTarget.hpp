9ff81a954a401e803a45488c75204cf1a3e706de
tfk8939 <tfk8939@rit.edu>
2022-02-28 16:10:35 -0500

    Adding ColorTarget Documentation

--- backend/src/ImageUtil/ColorTarget.hpp
+++ backend/src/ImageUtil/ColorTarget.hpp
@@ -13,13 +13,42 @@ typedef struct target_location {
 	int row_count, col_count;
 }TargetData;
 
+/**
+ * @brief Class that represents a ColorTarget from an image
+ * A Color target has a number of ColorPatches of different colors organized
+ * into rows and colums.
+ * 
+ */
 class ColorTarget {
+
 
 public:
 	ColorTarget() {}
 	ColorTarget(btrgb::Image* im, TargetData location_data);
+
+	/**
+	 * @brief Calculate the average pixel value for specified color patch
+	 * 
+	 * @param row the row of the color patch
+	 * @param col the col of the color patch
+	 * @param channel the color channel to get the avg from
+	 * @param sp the sample width as a percentage of the color patch width
+	 * @return float the average pixel value
+	 */
 	float get_patch_avg(int row, int col, int channel, double sp=0.3);
+	
+	/**
+	 * @brief Get the row count
+	 * 
+	 * @return int 
+	 */
 	int get_row_count();
+
+	/**
+	 * @brief Get the col count
+	 * 
+	 * @return int 
+	 */
 	int get_col_count();
 
 private:
@@ -33,7 +62,20 @@ private:
 	int row_height;
 	int col_width;
 
+	/**
+	 * @brief Calculate the center x postiton for the specified col
+	 * 
+	 * @param col 
+	 * @return int 
+	 */
 	int patch_posX(int col);
+
+	/**
+	 * @brief Calculate the center y position for the specified row
+	 * 
+	 * @param row 
+	 * @return int 
+	 */
 	int patch_posY(int row);
 	
 };
