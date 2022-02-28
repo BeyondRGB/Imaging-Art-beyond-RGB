8b54244d80e9f5f95a908a4be7da7eda30beb5dd
tfk8939 <tfk8939@rit.edu>
2022-02-28 14:24:55 -0500

    Added more documenation

--- backend/src/utils/color_convertions.hpp
+++ backend/src/utils/color_convertions.hpp
@@ -14,7 +14,14 @@ namespace btrgb{
         double L, a, b;
     }Lab_t;
 
-
+    /**
+     * @brief Convert xyz values to L*a*b* values
+     * 
+     * @param xyz xyz_t struct containing the x,y,z values
+     * @param wp the whitepoints that should be used for convertion, 
+     * these are based on the Illuminants and Standard observer
+     * @return btrgb::Lab_t struct containgin the L*, a*, b* values
+     */
     btrgb::Lab_t xyz_2_Lab(btrgb::XYZ_t xyz, WhitePoints* wp);
 
 };
