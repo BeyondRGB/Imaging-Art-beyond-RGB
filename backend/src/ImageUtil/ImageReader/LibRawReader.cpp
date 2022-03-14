3e22be2716a74465c0a027d76802cbbf1a9de8bd
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-03-14 15:54:00 -0400

    Fix BGR order and preview reading

--- backend/src/ImageUtil/ImageReader/LibRawReader.cpp
+++ backend/src/ImageUtil/ImageReader/LibRawReader.cpp
@@ -30,40 +30,40 @@ void LibRawReader::_configLibRawParams() {
         case UNPROCESSED:
             /* Keep as 16 bit. */
             opt->output_bps = 16;
+
             /* Do not use histogram brightness/color scaling */
             opt->no_auto_bright = 1;
             opt->no_auto_scale = 1;
+            
+            /* Do not subtract camera black level. */
+            opt->user_black = 0;
+
+            /* Linear */
+            opt->gamm[0] = 1.0;
+            opt->gamm[1] = 1.0;
+
+            /* Don't use white balance. */
+            opt->use_camera_wb = 0;
+            opt->use_auto_wb = 0;
+
+            /* Don't use camera matrix if present. */
+            opt->use_camera_matrix = 0;
+
+            /* Use raw color space. */
+            opt->output_color = 0;
+
             break;
 
         case PREVIEW:
             opt->output_bps = 8;
-            opt->no_auto_bright = 0;
-            opt->no_auto_scale = 0;
             opt->half_size = 1;
             break;
 
         default: throw std::logic_error("[LibRawReader] Invalid method.");
     }
 
-    /* Do not subtract camera black level. */
-    opt->user_black = 0;
-
-    /* Linear */
-    opt->gamm[0] = 1.0;
-    opt->gamm[1] = 1.0;
-
     /* Bi-linear interpolation. */
     opt->user_qual = 0;
-
-    /* Don't use white balance. */
-    opt->use_camera_wb = 0;
-    opt->use_auto_wb = 0;
-
-    /* Don't use camera matrix if present. */
-    opt->use_camera_matrix = 0;
-
-    /* Use raw color space. */
-    opt->output_color = 0;
 
     /* Use raw speed. */
     opt->use_rawspeed = 1;
