1f33c17e0c5aa0a4782506a20e51af2358c8047c
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-03-14 16:07:24 -0400

    Removed a temporary debug line

--- backend/src/ImageUtil/ImageWriter/ImageWriter.cpp
+++ backend/src/ImageUtil/ImageWriter/ImageWriter.cpp
@@ -8,8 +8,6 @@ namespace btrgb {
 
     void ImageWriter::write(Image* im, std::string filename) {
 
-        filename = "out/" + filename;
-
         std::string fname = filename;
         if (!fname.ends_with(this->file_extension))
             fname += this->file_extension;
