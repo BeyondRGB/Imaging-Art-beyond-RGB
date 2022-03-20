d34a2eb82cfda7d9abe9205d876405c91188b2bf
tfk8939 <tfk8939@rit.edu>
2022-03-20 13:07:48 -0400

    added documenation to TimeTracker

--- backend/src/utils/time_tracker.hpp
+++ backend/src/utils/time_tracker.hpp
@@ -4,13 +4,49 @@
 #include <iostream>
 #include <ctime>
 
+/**
+ * @brief Helper class for timing various tasks
+ * 
+ * To use 
+ *      - Create an instance of the TimeTracker
+ *      - start_timing()
+ *      - preform tasks to be timed
+ *      - end_timing()
+ *      - request elaped time
+ *  
+ * 
+ */
 class TimeTracker{
 public:
+    /**
+     * @brief Begin timing 
+     */
     void start_timeing();
+
+    /**
+     * @brief end timing 
+     */
     void end_timeing();
 
+    /**
+     * @brief Get total elapsed time in seconds
+     * 
+     * @return int 
+     */
     int elapsed_time_sec();
+
+    /**
+     * @brief Get total elapsed time in ms
+     * 
+     * @return int 
+     */
     int elapsed_time_ms();
+
+    /**
+     * @brief get total elapsed time in minutes
+     * 
+     * @return float 
+     */
     float elapsed_time_min();
 
 private:
