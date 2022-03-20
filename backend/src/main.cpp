3efc4c723ccd532d831133c0919d7dfbf6319c7f
tfk8939 <tfk8939@rit.edu>
2022-03-20 12:58:47 -0400

    Removing testing from main for PR

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -6,46 +6,8 @@
 
 
 //Testing Includes: Remove before submiting PR
-#include "reference_data/illuminants.hpp"
-#include "reference_data/ref_data.hpp"
-#include "reference_data/standard_observer.hpp"
-#include "utils/calibration_util.hpp"
-#include "utils/time_tracker.hpp"
-#include <chrono>
-#include <thread>
-
-
 void testFunc() {
-    // ObserverType so = StandardObserver::ObserverType::SO_1931;
-    // IlluminantType illum = Illuminants::IlluminantType::D50;
-    // std::string file = "APT_Reflectance_Data.csv";
-    // RefData ref_data(file, illum, so);
-    // cv::Mat m = ref_data.as_matrix();
-    // btrgb::calibration::display_matrix(&m, "RefData");
 
-    TimeTracker tracker;
-    tracker.start_timeing();
-    sleep_for(seconds(30));
-    tracker.end_timeing();
-    
-
-    tracker.elapsed_time_min();
-    tracker.elapsed_time_sec();
-    tracker.elapsed_time_ms();
-    
-    
-    // cv::Mat m = (cv::Mat_<double>(3, 5) << 
-    //                 3 ,  7, 1,  6,  2,
-    //                 10, 33, 5, 42, 14,
-    //                  -1,  2, 3,  4,  5
-
-    //                 );
-
-    // btrgb::calibration::display_matrix(&m, "TestMat");
-    // int row = 2;
-    // double min = btrgb::calibration::row_min(m, row);
-    // double max = btrgb::calibration::row_max(m, row);
-    // std::cout << "\nMin: " << min << " Max: " << max << std::endl;
 }
 
 
