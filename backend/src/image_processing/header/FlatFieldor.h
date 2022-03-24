1b48f6a9b33076f0e51035cf6909a8fb6fefbc67
tfk8939 <tfk8939@rit.edu>
2022-03-24 11:39:22 -0400

    Added Y and W values to General Info

--- backend/src/image_processing/header/FlatFieldor.h
+++ backend/src/image_processing/header/FlatFieldor.h
@@ -4,6 +4,7 @@
 
 #include "ImgProcessingComponent.h"
 #include "ImageUtil/Image.hpp"
+#include "image_processing/results/calibration_results.hpp"
 
 class FlatFieldor : public ImgProcessingComponent{
 private:
@@ -12,6 +13,7 @@ private:
     void pixelOperation(int h, int wid, int c, btrgb::Image* a1, btrgb::Image* a2, btrgb::Image* wh1, btrgb::Image* wh2, btrgb::Image* d1, btrgb::Image* d2);
 public:
     void execute(CommunicationObj* comms, btrgb::ArtObject* images) override;
+    void store_results(btrgb::ArtObject* images);
 };
 
 
