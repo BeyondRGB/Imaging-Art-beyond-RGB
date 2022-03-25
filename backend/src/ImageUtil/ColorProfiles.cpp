62b90f675850c0a3ef77285e8459e955ce877784
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-03-25 10:15:00 -0400

    Updated some conversions

--- backend/src/ImageUtil/ColorProfiles.cpp
+++ backend/src/ImageUtil/ColorProfiles.cpp
@@ -54,6 +54,27 @@ void ColorProfiles::convert_to_xyz(cv::Mat im, ColorSpace from) {
             0.0000000, 0.0000000, 0.8252100
         ); break;
 
+    case ColorSpace::sRGB:
+        m = (cv::Mat_<float>(3,3) <<
+            0.4360747, 0.3850649, 0.1430804,
+            0.2225045, 0.7168786, 0.0606169,
+            0.0139322, 0.0971045, 0.7141733
+        ); break;
+
+    case ColorSpace::Adobe_RGB_1998:
+        m = (cv::Mat_<float>(3,3) <<
+            0.6097559, 0.2052401, 0.1492240,
+            0.3111243, 0.6256560, 0.0632197,
+            0.0194811, 0.0608902, 0.7448386
+        ); break;
+
+    case ColorSpace::Wide_Gamut_RGB:
+        m = (cv::Mat_<float>(3,3) <<
+            0.7161046, 0.1009296, 0.1471858,
+            0.2581873, 0.7249378, 0.0168748,
+            0.0000000, 0.0517813, 0.7734287
+        ); break;
+
     default:
         throw std::runtime_error("[ColorProfiles::convert_to_xyz] Not implemented. ");
     }
@@ -106,11 +127,13 @@ void ColorProfiles::convert_to_color(cv::Mat im, ColorSpace to) {
         pixel[G] = BTRGB_CLIP_PIXEL(pixel[G]);
         pixel[B] = BTRGB_CLIP_PIXEL(pixel[B]);
     });
+    #undef BTRGB_CLIP_PIXEL
 }
 
 
 void ColorProfiles::linearize(cv::Mat im, ColorSpace from) {
     #define BTRGB_PROPHOTO_INVERSE_GAMMA(x) (x >= 0.03125 ? pow(x, 1.8) : x / 16)
+    #define BTRGB_sRGB_INVERSE_GAMMA(x) (x > 0.040449936 ? pow((x + 0.055) / 1.055, 2.4): x / 12.92)
 
     switch(from) {
 
@@ -121,17 +144,25 @@ void ColorProfiles::linearize(cv::Mat im, ColorSpace from) {
                 pixel[B] = BTRGB_PROPHOTO_INVERSE_GAMMA(pixel[B]);
             }); break;
 
+        case ColorSpace::sRGB:
+            im.forEach<cv::Vec3f>([](cv::Vec3f& pixel, const int* pos) -> void {
+                pixel[R] = BTRGB_sRGB_INVERSE_GAMMA(pixel[R]);
+                pixel[G] = BTRGB_sRGB_INVERSE_GAMMA(pixel[G]);
+                pixel[B] = BTRGB_sRGB_INVERSE_GAMMA(pixel[B]);
+            }); break;
+
         default:
             throw std::runtime_error("[ColorProfiles::inverse_gamma] Not implemented. ");
     }
         
     #undef BTRGB_PROPHOTO_INVERSE_GAMMA
+    #undef BTRGB_sRGB_INVERSE_GAMMA
 }
 
 
 void ColorProfiles::apply_gamma(cv::Mat im, ColorSpace to) {
     #define BTRGB_PROPHOTO_GAMMA(x) (x >= 0.001953125 ? pow(x, 1/1.8) : x * 16)
-    #define BTRGB_sRGB_GAMMA(x) (x > 0.0031308 ? 1.055 * pow(x, 1/2.5) - 0.055 : x * 12.92)
+    #define BTRGB_sRGB_GAMMA(x) (x > 0.0031308 ? 1.055 * pow(x, 1/2.4) - 0.055 : x * 12.92)
 
     int channels = im.channels();
     switch(to) {
