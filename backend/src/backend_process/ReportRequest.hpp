b4543da23b1759e6f20506e3cd71de24b9e028c5
Jake O'Neil <jgoneil03@gmail.com>
2022-04-04 17:13:27 -0400

    Forgot to push this up to the branch

---
+++ backend/src/backend_process/ReportRequest.hpp
@@ -0,0 +1,25 @@
+#ifndef REPORTREQUEST_H
+#define REPORTREQUEST_H
+
+#include "ImageUtil/Image.hpp"
+#include "utils/json.hpp"
+#include "server/comunication_obj.hpp"
+
+#include "backend_process.hpp"
+
+
+
+class ReportRequest : public BackendProcess {
+
+public:
+    ReportRequest();
+    ~ReportRequest();
+	void run() override;
+
+private:
+	static unsigned int id;
+
+};
+
+
+#endif
