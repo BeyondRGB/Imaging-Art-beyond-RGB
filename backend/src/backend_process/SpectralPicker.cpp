2a1ae3efd9116013a464a5192245593367d3c9b9
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-04-05 13:14:17 -0400

    Clean up

--- backend/src/backend_process/SpectralPicker.cpp
+++ backend/src/backend_process/SpectralPicker.cpp
@@ -43,47 +43,30 @@ void SpectralPicker::run() {
         int top = (y - radius < 0 ? 0 : y - radius);
         int bot = (y + radius > height ? height : y + radius);
 
-        std::cout << "\n\n\nsize: " << size << std::endl;
-        std::cout << "x: " << x << std::endl;
-        std::cout << "y: " << y << std::endl;
-        std::cout << "left: " << left << std::endl;
-        std::cout << "right: " << right << std::endl;
-        std::cout << "top: " << top << std::endl;
-        std::cout << "bottom: " << bot << std::endl;
-
         cv::Mat im = tiff_reader->getCrop(left, top, right - left, bot - top);
         int channels = im.channels();
-        cv::Mat avg_cam_sig(6, 1, CV_32FC1);
+        if(channels > 16) throw std::runtime_error("Image has more than 16 channels.");
 
-        float* avg = (float*) avg_cam_sig.data;
-        for(int z=0; z<6; z++) avg[z] = 0;
+        float avg[16] = {0};
         uint16_t* im_pixel;
 
         for (int row = 0; row < im.rows; row++) {
             for (int col = 0; col < im.cols; col++) {
                 im_pixel = (uint16_t*) im.data + row * im.cols * channels + col * channels;
-                avg[R1] += float(im_pixel[R1]);
-                avg[G1] += float(im_pixel[G1]);
-                avg[B1] += float(im_pixel[B1]);
-                avg[R2] += float(im_pixel[R2]);
-                avg[G2] += float(im_pixel[G2]);
-                avg[B2] += float(im_pixel[B2]);
+                for(int ch = 0; ch < channels; ch++)
+                    avg[ch] += float(im_pixel[ch]);
             }
         }
 
-        float num_pixels = float(0xffff) * float(im.rows * im.cols);
-        std::cout << "num_pixels: " << num_pixels << std::endl;
-        avg[R1] /= num_pixels;
-        avg[G1] /= num_pixels;
-        avg[B1] /= num_pixels;
-        avg[R2] /= num_pixels;
-        avg[G2] /= num_pixels;
-        avg[B2] /= num_pixels;
+        float total_and_normalizer = float(0xffff) * float(im.rows * im.cols);
+        for(int ch = 0; ch < channels; ch++)
+            avg[ch] /= total_and_normalizer;
 
+        cv::Mat avg_cam_sig(channels, 1, CV_32FC1, avg);
         cv::Mat m = tiff_reader->getConversionMatrix(BTRGB_M_REFL_OPT);
         cv::Mat spectrum = m * avg_cam_sig;
 
-        this->coms_obj_m->send_spectrum((float*)spectrum.data, spectrum.rows * spectrum.cols);
+        this->coms_obj_m->send_spectrum((float*)spectrum.data, spectrum.rows);
 
     }
     catch(const ParsingError& e) {
