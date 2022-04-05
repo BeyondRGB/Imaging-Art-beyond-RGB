43e5a9640625bf030e78384671a4f48c9f4c6b49
Jake O'Neil <jgoneil03@gmail.com>
2022-04-04 21:27:52 -0400

    Clean up and possible planning comment

--- backend/src/backend_process/ReportRequest.cpp
+++ backend/src/backend_process/ReportRequest.cpp
@@ -12,12 +12,11 @@ ReportRequest::~ReportRequest() {}
 
 void ReportRequest::run() {
     std::string filename = this->process_data_m->get_string("name");
-    //std::ifstream file(filename);
-    //std::string all_data((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     jsoncons::json convert_json = Jsonafiable::json_from_file(filename);
     Json all_json(convert_json);
     Json report_data = all_json.get_obj("CalibrationResults");
     Json verifcation_data = all_json.get_obj("VerificationResults");
+    //Need to add a way to make/adjust current CalibrationResults Obj, don't have access to art obj so can't use that one currently
     this->coms_obj_m->send_reports(report_data.get_jsoncons(), "Calibration");
     this->coms_obj_m->send_reports(verifcation_data.get_jsoncons(), "Verification");
 }
