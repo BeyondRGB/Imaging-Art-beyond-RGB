c7bb0c95136b6b9c757811be204904ea637c2387
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-04-05 01:47:28 -0400

    Calculate avg spectrum

--- backend/src/backend_process/SpectralPicker.cpp
+++ backend/src/backend_process/SpectralPicker.cpp
@@ -20,7 +20,7 @@ void SpectralPicker::run() {
         double y_rel = this->process_data_m->get_obj("coordinates").get_number("y");
         double size_rel = this->process_data_m->get_number("size");
         std::ifstream prj_file(prj_filename);
-        std::string local_file = jsoncons::json::parse(prj_file)["OutPutFiles"]["CM"].as<std::string>();
+        std::string local_file = jsoncons::json::parse(prj_file)["OutPutFiles"]["SP"].as<std::string>();
         std::string filename = prj_filename.substr(0, prj_filename.find_last_of("/\\") + 1) + local_file;
 
         if( ! btrgb::Image::is_tiff(filename) )
@@ -34,10 +34,40 @@ void SpectralPicker::run() {
         int y = (y_rel * rel) - (size / 2);
 
         cv::Mat im = tiff_reader->getCrop(x, y, size, size);
+        cv::Mat avg_cam_sig(6, 1, CV_32FC1);
+
+        float* avg = (float*) avg_cam_sig.data;
+        uint16_t* im_pixel = (uint16_t*) im.data;
+
+        for (int row = 0; row < im.rows; row++) {
+            for (int col = 0; col < im.cols; col++) {
+                im_pixel += (row * im.cols + col);
+                avg[R1] += im_pixel[R1];
+                avg[G1] += im_pixel[G1];
+                avg[B1] += im_pixel[B1];
+                avg[R2] += im_pixel[R2];
+                avg[G2] += im_pixel[G2];
+                avg[B2] += im_pixel[B2];
+            }
+        }
+
+        float total = float(0xffff) * float(im.rows * im.cols);
+        avg[R1] /= total;
+        avg[G1] /= total;
+        avg[B1] /= total;
+        avg[R2] /= total;
+        avg[G2] /= total;
+        avg[B2] /= total;
+
+
         cv::Mat m = tiff_reader->getConversionMatrix(BTRGB_M_REFL_OPT);
+        cv::Mat spectrum = m * avg_cam_sig;
 
-
-
+        std::cout << "rows: " << spectrum.rows << std::endl;
+        std::cout << "cols: " << spectrum.cols << std::endl;
+        avg = (float*) spectrum.data;
+        for(int row = 0; row < spectrum.rows; row++)
+            std::cout << avg[row] << std::endl;
 
     }
     catch(const ParsingError& e) {
