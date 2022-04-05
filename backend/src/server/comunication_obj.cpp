3f44b6e0da36dd286bf554a21b64c83a33e0db89
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-04-05 11:26:38 -0400

    Ready for pat, but needs clean up.

--- backend/src/server/comunication_obj.cpp
+++ backend/src/server/comunication_obj.cpp
@@ -142,6 +142,21 @@ void CommunicationObj::send_reports(jsoncons::json reports) {
 	send_msg(all_info);
 }
 
+void CommunicationObj::send_spectrum(float* data, int size) {
+	jsoncons::json info_body;
+	info_body.insert_or_assign("RequestID", id);
+	info_body.insert_or_assign("ResponseType", "SpectralPicker");
+	jsoncons::json response_data;
+	response_data["size"] = size;
+	response_data["spectrum"] = jsoncons::json::make_array(size);
+	for( int i=0; i<size; i++)
+		response_data["spectrum"][i] = data[i];
+	info_body.insert_or_assign("ResponseData", response_data);
+	std::string all_info;
+	info_body.dump(all_info);
+	send_msg(all_info);
+}
+
 
 btrgb::base64_ptr_t CommunicationObj::createDataURL(enum btrgb::output_type type, std::vector<uchar>* direct_binary) {
 
