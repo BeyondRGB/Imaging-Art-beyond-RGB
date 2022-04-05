e0f1ab1f2219d717cd0c0d7c48eedf56443cd8a8
AceHammer17 <arh6766@g.rit.edu>
2022-04-05 16:13:32 -0400

    InitalWork on blending dead pixels

--- backend/src/image_processing/header/FlatFieldor.h
+++ backend/src/image_processing/header/FlatFieldor.h
@@ -10,7 +10,7 @@ class FlatFieldor : public ImgProcessingComponent{
 private:
     float w;
     void wCalc(float pAvg, float wAvg, double yRef);
-    void pixelOperation(int h, int wid, int c, btrgb::Image* a1, btrgb::Image* a2, btrgb::Image* wh1, btrgb::Image* wh2, btrgb::Image* d1, btrgb::Image* d2);
+    void pixelOperation(int h, int wid, int c, btrgb::Image* a1, btrgb::Image* a2, btrgb::Image* wh1, btrgb::Image* wh2, btrgb::Image* d1, btrgb::Image* d2, btrgb::Image* a1c);
 public:
     void execute(CommunicationObj* comms, btrgb::ArtObject* images) override;
     void store_results(btrgb::ArtObject* images);
