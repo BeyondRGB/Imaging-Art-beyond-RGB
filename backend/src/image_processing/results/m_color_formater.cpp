b46906c048b5f676042166b6074cbb9610ea9b18
tfk8939 <tfk8939@rit.edu>
2022-04-08 08:12:21 -0400

    M_spectral

--- backend/src/image_processing/results/m_color_formater.cpp
+++ backend/src/image_processing/results/m_color_formater.cpp
@@ -7,11 +7,15 @@ void MColorFormater::write_format(std::ostream &output_stream, CalibrationResult
     try{
         M = results->get_matrix(CM_M);
         results->write_matrix(output_stream, M, "M_color");
-    }catch(ParsingError e){ output_stream << e.what() << std::endl; }
+    }catch(ParsingError e){ 
+        output_stream << e.what() << std::endl; 
+    }
     try{
         offset = results->get_matrix(CM_OFFSETS);
         results->write_matrix(output_stream, offset, "M_color_offset");
-    }catch(ParsingError e){ output_stream << e.what() << std::endl; }
+    }catch(ParsingError e){ 
+        output_stream << e.what() << std::endl; 
+    }
 
 
 }
