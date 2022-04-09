19c5437975a5ab639eaf340d068ef062545f3724
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-04-09 16:09:26 -0400

    Change to LeafComponent update

--- backend/src/image_processing/header/NoiseReduction.h
+++ backend/src/image_processing/header/NoiseReduction.h
@@ -3,12 +3,13 @@
 #define BEYOND_RGB_BACKEND_NOISEREDUCTION_H
 
 
-#include "ImgProcessingComponent.h"
+#include "image_processing/header/LeafComponent.h"
 
-class NoiseReduction : public ImgProcessingComponent {
+class NoiseReduction : public LeafComponent {
 private: std::string SharpenFactor;
 public:
-    NoiseReduction(std::string SharpenFactor) :SharpenFactor(SharpenFactor) {};
+    ~NoiseReduction() {};
+    NoiseReduction(std::string SharpenFactor) : LeafComponent("NoiseReduction"), SharpenFactor(SharpenFactor) {};
     void execute(CommunicationObj* comms, btrgb::ArtObject* images) override;
 };
 
