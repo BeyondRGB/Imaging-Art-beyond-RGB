ad55e5eb28aa47ba2efc513544d474deac524a4d
tfk8939 <tfk8939@rit.edu>
2022-04-10 16:15:14 -0400

    Applying registration to targets

--- backend/src/image_processing/header/PixelRegestor.h
+++ backend/src/image_processing/header/PixelRegestor.h
@@ -3,11 +3,14 @@
 #define BEYOND_RGB_BACKEND_PIXELREGESTOR_H
 
 #include "image_processing/header/LeafComponent.h"
+#include "ImageUtil/Image.hpp"
 
 class PixelRegestor : public LeafComponent{
 public:
     PixelRegestor() : LeafComponent("PixelRegestor"){}
     void execute(CommunicationObj* comms, btrgb::ArtObject* images) override;
+    void appy_regestration(CommunicationObj* comms, btrgb::Image *img1, btrgb::Image *img2, int cycle, int cycle_count);
+    float calc_progress(float progress, float cycle, float cycle_count);
 };
 
 #endif //BEYOND_RGB_BACKEND_PIXELREGESTOR_H
