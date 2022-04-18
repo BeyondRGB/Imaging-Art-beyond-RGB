f386cf1eaeb6d8b11f227a3e32d44537a21613c7
Jared Ponzetti <35544800+Jared-Ponzetti@users.noreply.github.com>
2022-04-18 09:28:41 -0400

    Update pipeline renaming

--- backend/src/image_processing/cpp/ImageProcessor.cpp
+++ backend/src/image_processing/cpp/ImageProcessor.cpp
@@ -1,7 +1,7 @@
 #include "../header/ImageProcessor.h"
 
 ImageProcessor::ImageProcessor(const std::vector<std::shared_ptr<ImgProcessingComponent>> &components)
-    : CompositComponent("Image Processing") {
+    : CompositComponent("Profiler") {
         this->init_components(components);
 }
 void ImageProcessor::execute(CommunicationObj* comms, btrgb::ArtObject* images) {
