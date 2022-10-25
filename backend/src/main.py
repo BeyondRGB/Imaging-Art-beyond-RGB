d82bff7c58c3ec5d44672b7fc3221457324169d0
Brendan Grau <bsg8376@rit.edu>
2022-10-25 16:06:35 -0400

    Add main.py prototype

---
+++ backend/src/main.py
@@ -0,0 +1,22 @@
+""" preprocessing.py
+App init functions
+
+Functions:
+    main()
+
+Authors:
+    Brendan Grau <bsg8376@rit.edu>
+
+License:
+    © 2022 BeyondRGB
+    This code is licensed under the MIT license (see LICENSE.txt for details)
+"""
+import sys
+
+
+def main():
+    """ App entry point """
+
+
+if __name__ == "__main__":
+    main()
