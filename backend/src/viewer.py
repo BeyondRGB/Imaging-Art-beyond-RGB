0513fa689a2cb4e7455b0b856fc52f6a62c40e17
Brendan Grau <bsg8376@g.rit.edu>
2022-12-07 18:27:55 -0500

    store point

--- backend/src/viewer.py
+++ backend/src/viewer.py
@@ -7,6 +7,14 @@ def settings(*args):
 
     form.edit()
 
+    form = npyscreen.Form(name='Select Files')
+    for i in range(0, int(num_images.value)):
+        file = form.add(npyscreen.TitleFilename, name="File:")
+
+    form.edit()
+
+    return num_images.value
+
 
 if __name__ == '__main__':
     print(npyscreen.wrapper_basic(settings))
