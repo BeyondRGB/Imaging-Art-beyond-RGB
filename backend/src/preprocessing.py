8f15452859e046052de16694d4a2dfcd3e4c4b06
eli <egc2846@rit.edu>
2023-01-31 20:01:25 -0500

    move to pc

--- backend/src/preprocessing.py
+++ backend/src/preprocessing.py
@@ -20,7 +20,7 @@ License:
 # Python imports
 import numpy as np
 from cv2 import medianBlur, cvtColor, COLOR_RGB2GRAY, BFMatcher,\
-        findHomography, warpPerspective, RANSAC, ORB_create, NORM_HAMMING, imshow, waitKey, COLOR_GRAY2RGB, SIFT_create, normalize, NORM_MINMAX
+        findHomography, warpPerspective, RANSAC, ORB_create, NORM_L1, imshow, waitKey, COLOR_GRAY2RGB, SIFT_create, normalize, NORM_MINMAX
 
 # Local imports
 from constants import BLUR_FACTOR, TARGET_RADIUS, Y_VAL
@@ -186,7 +186,7 @@ def registration(packet):
     print(descriptors1)
 
     # match images
-    matcher = BFMatcher(NORM_HAMMING, crossCheck = True)
+    matcher = BFMatcher(NORM_L1, crossCheck = False)
     matches = matcher.match(descriptors1, descriptors2)
     matches = tuple(sorted(matches, key=lambda x: x.distance))
     matches = matches[:int(len(matches) * 0.9)]
