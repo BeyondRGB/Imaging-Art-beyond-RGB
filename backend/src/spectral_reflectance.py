1c4d8729df92ca3acd6d1a95769bf27a4676b8a7
keenanmiller <keenan@marginedge.com>
2023-03-01 12:42:35 -0500

    spectral reflectance sorta works

--- backend/src/spectral_reflectance.py
+++ backend/src/spectral_reflectance.py
@@ -13,28 +13,55 @@ from scipy.linalg import pinv
 import numpy as np
 
 # Local Imports
-from constants import INIT_M_REFL
+from constants import INIT_M_REFL, my_data
+from packet import Packet
 
 
-def spectrally_transform(packet):
+def spectrally_transform(packet: Packet):
+    r_reference = np.genfromtxt('NGT_spectral_reflectance.csv', delimiter=',')
+    r_reference[...] = np.reshape(r_reference, (36, 130))
+    initial_guess = np.ndarray.flatten(np.full((36,6), .2))
+    xopt, fopt, iter, funcalls, warnflag = fmin(func=__eq, x0=initial_guess, args=(a, r_reference), maxfun=500000, maxiter=500000, full_output=True)
+    print('Spectral transformation fopt: ' + str(fopt))
+    # TODO use above results
+    return
 
-    initial_guess = np.ndarray.flatten(INIT_M_REFL)
-    res = fmin(func=eq, x0=initial_guess, args=(packet.camsigs, None))
 
-
-def eq(x, camsigs, nothing):
-    """ Equation to minimize """
+def __eq(x: np.ndarray, camsigs: np.ndarray, r_reference: np.ndarray) -> float:
+    """ Spectral Reflectance equation to minimize
+    [in] x : The current guess array
+    [in] camsigs : The camera signals extracted from the preprocessed color target
+    [r_reference] : The 'actual' reference data for the given color target
+    """
 
     x = np.reshape(x, (36, 6))
     r_camera = np.matmul(x, camsigs)
 
     global_maximums = []
     global_minimums = []
+    global_maximums_ref = []
+    global_minimums_ref = []
     for i in range(0, 36):
         row = r_camera[i]
         global_maximums.append(np.amax(row))
         global_minimums.append(np.amin(row))
-    print(str(global_maximums))
-    print(str(global_minimums))
 
-    return 0
+        row = r_reference[i]
+        global_maximums_ref.append(np.amax(row))
+        global_minimums_ref.append(np.amin(row))
+
+    r_diff = np.subtract(r_reference, r_camera)
+    r_diff_square = np.square(r_diff)
+    e1 = np.sum(r_diff_square)**.5
+
+    r_max_diff = np.subtract(global_maximums, global_maximums_ref)
+    r_max_diff_square = np.square(r_max_diff)
+    e2 = np.sum(r_max_diff_square)
+
+    r_min_diff = np.subtract(global_minimums, global_minimums_ref)
+    r_min_diff_square = np.square(r_min_diff)
+    e3 = np.sum(r_min_diff_square)
+
+    Z = e1 + 10*e2 + 50*e3
+
+    return Z
