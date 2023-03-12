ea5cded801c154ade22fed90a960c094696620fe
Sungmin Kim <sk4900@rit.edu>
2023-03-12 18:06:08 -0400

    truncated btrgb file to only important values

--- backend/src/btrgb.py
+++ backend/src/btrgb.py
@@ -11,8 +11,9 @@ License:
     This code is licensed under the MIT license (see LICENSE.txt for details)
 """
 import json
+from lab_refs import LAB_REF
 
-class btrgb:
+class Btrgb:
     """
     Class representing values to be output to btrgb file.
 
@@ -20,13 +21,11 @@ class btrgb:
         output_files             : List of files to be outputted
         calibration_results      : Dict of calibration values such as DeltaE values
         verification_results     : Dict of verification values
-        general_info             : Dict of misc values about raw photo and target
 
     Methods: 
         set_output_files         : Set output files
         set_calibration_results  : Set calibration results
         set_verification_results : Set verification results
-        set_general_info         : Set general info
         to_json                  : Helper function returning a json object representing a btrgb class
         write_to_file            : Write btrgb class to a file in json format
     """
@@ -36,36 +35,39 @@ class btrgb:
     verification_results = {}
     general_info = {}
 
-    def set_output_files(self, output_files):
-        self.output_files = output_files
+    def __init__(self):
+        self.calibration_results = {
+            "double_values": [],
+            "matrix_values": []
+        }
+        self.create_matrix_value(LAB_REF[0], "CM L*_ref")
+        self.create_matrix_value(LAB_REF[1], "CM a*_ref")
+        self.create_matrix_value(LAB_REF[2], "CM b*_ref")
 
-    def set_calibration_results(self, double_values,
-                                integer_values, matrix_values, string_values):
-        self.calibration_results.DOUBLE_VALUES = double_values
-        self.calibration_results.INTEGER_VALUES = integer_values
-        self.calibration_results.MATRIX_VALUES = matrix_values
-        self.calibration_results.STRING_VALUES = string_values
+    def create_matrix_value(self, matrix, name):
+        matrix_value = {
+            "rows": 10,
+            "cols": 13,
+            "data": list(matrix),
+            "name": name
+        }
+        self.calibration_results["matrix_values"].append(matrix_value)
 
-    def set_verification_results(self, double_values,
-                                integer_values, matrix_values, string_values):
-        self.verification_results.DOUBLE_VALUES = double_values
-        self.verification_results.INTEGER_VALUES = integer_values
-        self.verification_results.MATRIX_VALUES = matrix_values
-        self.verification_results.STRING_VALUES = string_values
-
-    def set_general_info(self, double_values,
-                        integer_values, matrix_values, string_values):
-        self.general_info.DOUBLE_VALUES = double_values
-        self.general_info.INTEGER_VALUES = integer_values
-        self.general_info.MATRIX_VALUES = matrix_values
-        self.general_info.STRING_VALUES = string_values
-
-    def to_json(self):
-        return json.dumps(self, default=lambda obj: obj.__dict__,
-                        sort_keys=True, indent=4)
+    def create_double_value(self, double, name):
+        double_value = {
+            "data": double,
+            "name": name
+        }
+        self.calibration_results["double_values"].append(double_value)
+        
+    def create_output_file(self, file, name):
+        self.output_files[name]= file
 
     def write_to_file(self, id):
-        f = open(f"BeyondRGB_{id}.btrgb", "w")
-        f.write(self.to_json)
+        contents = json.dumps(self, default=lambda obj: obj.__dict__,
+                        sort_keys=True, indent=4)
+        with open(f"BeyondRGB_{id}.btrgb", "w", encoding='utf-8') as f:
+            f.write(contents)
 
 
+btrgb_file = Btrgb()
--- backend/src/main.py
+++ backend/src/main.py
@@ -18,6 +18,7 @@ import argparse
 from rgbio import save_image
 from pipeline import processing_pipeline
 from packet import genpacket, gentarget, getimg
+from btrgb import btrgb_file
 from parser import Parser
 
 
@@ -76,12 +77,15 @@ def main():
 
     target = gentarget(top_left, bottom_right, (int(args.white_row),
                                                 int(args.white_col)))
-
+    
     # Setup packet
     packet = build_packet(args.images, target, args.outpath)
 
+
     # Begin pipeline
     processing_pipeline(packet)
+    print(packet)
+    btrgb_file.write_to_file("123")
 
 
 def build_packet(images, target, outpath):
