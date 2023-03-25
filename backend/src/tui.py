c1fbea9a7cd7795f9d3404b9828c6e93956bbac2
Brendan Grau <bsg8376@g.rit.edu>
2023-03-25 15:27:35 -0400

    implement scrolling

--- backend/src/tui.py
+++ backend/src/tui.py
@@ -1,44 +1,49 @@
 import curses
 from dataclasses import dataclass
 
-
-files = ['target1', 'target2', 'white1', 'white2', 'dark1', 'dark2', 'art1',
-         'art2']
-selects = [('Target A', ''), ('Target B', ''), ('Flat Field A', ''),
-           ('Flat Field B', ''), ('Dark Field A', ''), ('Dark Field B', ''),
-           ('Art A', ''), ('Art B', '')]
-
+RESERVED_LINES = 4  # Lines taken up by UI
+VERT_OFFSET = 3  # Vertical offset for printing column data
 
 @dataclass
 class __Tui:
-    """ Struct related to tui opperations
+    """ Struct related to tui operations
     Members:
         idxs        : list of selected indices in each column
         col_idx     : currently selected column
         max_idxs    : list of maximum indices in each column
         max_col_idx : maximum number of columns
         col_data    : data for each column (each idx represents 1 column)
+        scroll_idxs : current scrolling indicies for each column
     """
     idxs: list
     col_idx: int
     max_idxs: list
     max_col_idx: int
     col_data: list
+    scroll_idxs: list
 
 
 def tui(args):
-    # TODO remove
-    import random
-    random.shuffle(files)
+    files = ['target1', 'target2', 'white1', 'white2', 'dark1', 'dark2', 'art1',
+             'art2', 'art3', 'art4', 'art5', 'art6', 'art7', 'art8', 'art9',
+             'art10', 'art11', 'art12', 'art13', 'art14', 'art15', 'art16']
+    selects = [('Target A', ''), ('Target B', ''), ('Flat Field A', ''),
+               ('Flat Field B', ''), ('Dark Field A', ''), ('Dark Field B', ''), 
+               ('Art 1A', ''), ('Art 1B', ''), ('Art 2A', ''), ('Art 2B', ''),
+               ('Art 3A', ''), ('Art 3B', ''), ('Art 4A', ''), ('Art 4B', ''),
+               ('Art 5A', ''), ('Art 5B', ''), ('Art 6A', ''), ('Art 6B', ''),
+               ('Art 7A', ''), ('Art 7B', ''), ('Art 8A', ''), ('Art 8B', '')]
 
     # Init
-    tui = __Tui([0, 0], 0, [len(files), len(selects)], 2, [])  # 2 columns
     stdscr = __init_curses()
+    tui = __Tui([0, 0], 0, [len(files), len(selects)],
+                2, [files, selects], [0, 0])
 
+    # Run
     try:
         __run_sorter(stdscr, tui)
-    except:
-        print('error')
+    except Exception:
+        print('Tui error')
     finally:
         __cleanup_curses(stdscr)
 
@@ -49,29 +54,37 @@ def __run_sorter(stdscr, tui: __Tui):
         c = stdscr.getch()
         if c == ord('q'):
             break
-        elif c == curses.KEY_DOWN:
-            col_idx = tui.col_idx
-            tui.idxs[col_idx] = (tui.idxs[col_idx] + 1) % tui.max_idxs[col_idx]
-        elif c == curses.KEY_UP:
-            col_idx = tui.col_idx
-            tui.idxs[col_idx] = (tui.idxs[col_idx] - 1) % tui.max_idxs[col_idx]
-        elif c == curses.KEY_ENTER or c == 10 or c == 13:  # ENTER pressed
-            __handle_enter_sorter(tui)
+        else:
+            __keypress(c, tui)
 
         __draw_sorter(stdscr, tui)
 
 
-def __handle_enter_sorter(tui: __Tui):
-    f = files.pop(tui.idxs[0])
-    selects[tui.idxs[1]] = selects[tui.idxs[1]][0], f
-    tui.idxs[1] += 1
-    tui.max_idxs[0] -= 1
-    if tui.max_idxs[0] != 0 and tui.idxs[0] >= tui.max_idxs[0]:
-        tui.idxs[0] -= 1
+def __keypress(c: int, tui: __Tui):
+    if c == curses.KEY_DOWN:
+        col_idx = tui.col_idx
+        if tui.max_idxs[col_idx] > 0:
+            tui.idxs[col_idx] = (tui.idxs[col_idx] + 1) % tui.max_idxs[col_idx]
+    elif c == curses.KEY_UP:
+        col_idx = tui.col_idx
+        if tui.max_idxs[col_idx] > 0:
+            tui.idxs[col_idx] = (tui.idxs[col_idx] - 1) % tui.max_idxs[col_idx]
+    elif c == curses.KEY_ENTER or c == 10 or c == 13:  # ENTER pressed
+        if tui.max_idxs[0] == 0:
+            return  # There's nothing to press enter on
+        f = tui.col_data[0].pop(tui.idxs[0])
+        tui.col_data[1][tui.idxs[1]] = (tui.col_data[1][tui.idxs[1]][0], f)
+        # Update indices
+        tui.idxs[1] += 1
+        tui.max_idxs[0] -= 1
+        # Only decrement left col idx if it would become invalid otherwise
+        if tui.idxs[0] >= tui.max_idxs[0]:
+            tui.idxs[0] -= 1
 
 
 def __draw_sorter(stdscr, tui: __Tui):
     h, w = stdscr.getmaxyx()
+    max_entries = ((h - RESERVED_LINES) / 2) - 1
 
     stdscr.clear()
 
@@ -82,21 +95,57 @@ def __draw_sorter(stdscr, tui: __Tui):
     stdscr.addstr(1, 1, 'Selected Images')
     stdscr.addstr(1, int((w-2)/2), 'Ordered Images')
 
+    __update_scroll_idxs(stdscr, tui, 0)
+    __update_scroll_idxs(stdscr, tui, 1)
+
     # File list
-    for i, f in enumerate(files):
+    start_idx = tui.scroll_idxs[0]
+    for i, f in enumerate(tui.col_data[0]):
+        if i < start_idx or i > start_idx + max_entries:
+            continue
+        s_x = 3
+        s_y = 2*(i-start_idx)+VERT_OFFSET
         if tui.col_idx == 0 and i == tui.idxs[0]:
-            stdscr.addstr((2*i)+3, 3, f, curses.color_pair(1))
+            stdscr.addstr(s_y, s_x, f, curses.color_pair(1))
         else:
-            stdscr.addstr((2*i)+3, 3, f)
+            stdscr.addstr(s_y, s_x, f)
 
     # Selected list
-    for i, s in enumerate(selects):
+    start_idx = tui.scroll_idxs[1]
+    for i, s in enumerate(tui.col_data[1]):
+        if i < start_idx or i > start_idx + max_entries:
+            continue
         txt = s[0] + ': ' + s[1]
-        x = int((w-2)/2) + 2
+        s_x = int((w-2)/2) + 2
+        s_y = 2*(i-start_idx)+VERT_OFFSET
         if tui.col_idx == 1 and i == tui.idxs[1]:
-            stdscr.addstr((2*i)+3, x, txt, curses.color_pair(1))
+            stdscr.addstr(s_y, s_x, txt, curses.color_pair(1))
         else:
-            stdscr.addstr((2*i)+3, x, txt)
+            stdscr.addstr(s_y, s_x, txt)
+
+
+def __update_scroll_idxs(stdscr, tui: __Tui, col: int):
+    if tui.max_idxs[col] == 0:
+        return  # Nothing to do
+
+    # Calculate number of entries we can hold
+    h, w = stdscr.getmaxyx()
+    max_entries = ((h - RESERVED_LINES) / 2) - 1
+
+    cur_idx = tui.idxs[col]
+    max_idx = tui.max_idxs[col]
+    scroll = tui.scroll_idxs[col]
+
+    # Calculate start_idx
+    if cur_idx == 0:  # needed for wrapping
+        tui.scroll_idxs[col] = 0
+    elif cur_idx == max_idx-1 and max_idx > max_entries:
+        tui.scroll_idxs[col] = int(max_idx-max_entries-1)
+    elif scroll+max_entries-3 < cur_idx and scroll+max_entries < max_idx-1:
+        tui.scroll_idxs[col] = (scroll+1) % max_idx
+    elif cur_idx > 3 and tui.scroll_idxs[col]+3 > cur_idx:
+        tui.scroll_idxs[col] = (scroll-1) % max_idx
+
 
 
 def __init_curses():
