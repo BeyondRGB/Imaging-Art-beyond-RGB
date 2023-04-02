c909298d7b22eba95bcce0852fc9ec87d9ab9161
Brendan Grau <bsg8376@g.rit.edu>
2023-04-01 20:59:30 -0400

    add cpp stuff

---
+++ backend/src/utils/reader.cpp
@@ -0,0 +1,65 @@
+#include <libraw/libraw.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+void* read_image(std::string path)
+{
+        // Init
+        LibRaw reader;
+        // reader params
+        libraw_output_params_t* opt = &(reader.imgdata.params);
+        opt->user_black = 0;
+        opt->use_camera_wb = 0;
+        opt->use_auto_wb = 0;
+        opt->use_camera_matrix = 0;
+        opt->output_bps = 16;
+        opt->no_auto_bright = 1;
+        opt->no_auto_scale = 1;
+        opt->gamm[0] = 1.0;
+        opt->gamm[1] = 1.0;
+        opt->output_color = 0;
+        opt->user_qual = 0;
+
+        // Read
+        int error_code;
+        error_code = reader.open_file(path.c_str());
+        if(error_code) {
+                reader.recycle();
+                std::cout << "ERROR: failed to open file" << std::endl;
+        }
+        error_code = reader.unpack();
+        if(error_code) 
+                std::cout << "ERROR: failed to unpack file" << std::endl;
+        error_code = reader.dcraw_process();
+        if(error_code)
+                std::cout << "ERROR: failed to process file" << std::endl;
+        int width, height, channels, depth;
+        reader.get_mem_image_format(&width, &height, &channels, &depth);
+
+        std::cout << width << std::endl;
+        std::cout << height << std::endl;
+        std::cout << channels << std::endl;
+        std::cout << depth << std::endl;
+        
+        std::ofstream file;
+        file.open ("t1.txt");
+        for (int i = 0; i < width * height * channels; i++)
+                file << reader.imgdata.rawdata.raw_image[i] << ' ';
+        std::cout << std::endl;
+        file.close();
+
+
+        // Cleanup
+        reader.recycle();
+
+        // Return
+        return 0;
+}
+
+
+int main() 
+{
+        read_image("./SampleImages/targets_1.ARW");
+}
--- backend/src/utils/rponly.py
+++ backend/src/utils/rponly.py
@@ -3,5 +3,9 @@ import rawpy as rp
 
 def rponly(path):
     raw = rp.imread(path)
-    rgb = raw.postprocess()
-    return rgb
+    return raw.postprocess(use_camera_wb=False,
+                           use_auto_wb=False,
+                           no_auto_bright=True,
+                           output_color=rp.ColorSpace(0),
+                           user_black=0,
+                           output_bps=16).astype('f4')
