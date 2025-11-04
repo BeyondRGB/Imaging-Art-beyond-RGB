12e913de99fd6d4f6714bdeb095a212359f63061
Raynard Miot <rayhmiot@gmail.com>
2025-11-04 16:30:22 -0500

    added clarification comment

--- frontend/src/renderer/components/Process/ColorTargetViewer.svelte
+++ frontend/src/renderer/components/Process/ColorTargetViewer.svelte
@@ -195,6 +195,12 @@
                         right: Math.abs(eViewX - rightPos),
                         bottom: Math.abs(eViewY - botPos),
                     };
+
+                    // Looking at the code, I would guess that this block is supposed to add/remove rows and columns
+                    // from the color and verification targets. Perhaps the user could've pressed a + or - button somewhere
+                    // that would trigger this? As it is now, it doesn't do anything, since no elements within the
+                    // viewport have the class names required to pass any of the if-statements, namely
+                    // "inc", "dec", "top", "bottom", "left", or "right".
                     e.originalEvent.path.forEach((element) => {
                         if (element.classList?.length > 0) {
                             if (element.classList[0] === "inc") {
