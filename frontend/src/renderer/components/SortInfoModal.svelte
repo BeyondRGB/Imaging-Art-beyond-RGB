678e9f9b0f19df66ad47d7a1341ce1b49acb1bcb
Jack Ganger-Spivak <jack.gs.code@gmail.com>
2025-11-06 15:31:16 -0500

    Modal looks good, just need to add message to batch processing screen

--- frontend/src/renderer/components/SortInfoModal.svelte
+++ frontend/src/renderer/components/SortInfoModal.svelte
@@ -5,16 +5,20 @@
    
 <main>
     <div class="infomodal">
-        <h2>
-            Not enough recognizable image filename suffixes were found, <br>the program will attempt to sort by name but the selected roles may not be accurate.<br>
-                Try renaming your images to match the guide on this page for better sorting results.
-        </h2>
-        <h2>Image file naming conventions:</h2>
+        <h1>
+            Not enough recognizable image filename suffixes were found, the program will attempt to sort by name but the selected roles may not be accurate.
+                
+        </h1>
         <p class="section">
-            Filename format: anyFilename-typeIndicator_imageID.extension
+          Try renaming your images to match the guide on this page for better sorting results.
         </p>
+        <div class="mono_text">anyFilename-typeIndicator_imageID.extension</div>
         <p class="section">
-            Where type indicator is one of the types listed below and imageID is A or B
+            anyFilename can be any identifier you want.<br>
+            
+            ImageID can also be anything but must match with the associated images, i.e. the associated object, color target, flatfield, and darkfield images should all have the same ImageID. Use A or B if you are unsure.<br>
+
+            Type indicator is one of the types listed below.
         </p>
         <p>
             Image: "image", "print", "object", "art", "exhibit", "paint", "subject", "item", "obj"<br>
@@ -23,9 +27,7 @@
             Darkfield: "darkfield", "d", "darkcurrent", "dc", "black", "dark", "current", "signal", ”internal", "camera"<br>
             
         </p>
-        <p class="section">
-            <!-- <button class="closeDia" autofocus on:click={closeModal}>Confirm</button> -->
-        </p>
+        
     </div>
     
 </main>
@@ -39,14 +41,21 @@
   }
 
   h1 {
-    @apply text-2xl font-semibold text-gray-100 mb-2;
+    @apply text-xl font-semibold text-gray-100 mb-2;
+  }
+  h2 {
+    @apply text-sm font-medium text-gray-100 tracking-wider mb-2;
+  }
+
+  p {
+    @apply text-gray-400;
   }
 
   .section {
     @apply flex flex-col gap-4;
   }
 
-  h2 {
-    @apply text-sm font-medium text-gray-400 uppercase tracking-wider mb-2;
+  .mono_text {
+    @apply font-mono flex flex-col gap-4 text-gray-100 text-center;
   }
 </style>
