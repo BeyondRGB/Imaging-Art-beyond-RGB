a5893b0d43ecd0620b5486317c5861eb7633e948
pnoffke64 <parker.noffke@gmail.com>
2026-01-21 21:49:25 -0500

    test backend hook

--- backend/src/backend_process/ReportRequest.cpp
+++ backend/src/backend_process/ReportRequest.cpp
@@ -1,9 +1,8 @@
-#include <regex>
-#include <cpptrace/cpptrace.hpp>
 #include "backend_process/ReportRequest.hpp"
+#include <cpptrace/cpptrace.hpp>
+#include <regex>
 
 ReportRequest::~ReportRequest() {}
-
 
 void ReportRequest::run() {
     try {
@@ -12,12 +11,16 @@ void ReportRequest::run() {
         Json all_json(convert_json);
         Json report_data = all_json.get_obj("CalibrationResults");
         Json verifcation_data = all_json.get_obj("VerificationResults");
-        //Need to add a way to make/adjust current CalibrationResults Obj, don't have access to art obj so can't use that one currently
-        this->coms_obj_m->send_reports(report_data.get_jsoncons(), "Calibration");
-        this->coms_obj_m->send_reports(verifcation_data.get_jsoncons(), "Verification");
-    }
-    catch(const std::exception& e) {
-        this->coms_obj_m->send_error("[ReportRequest] Invalid request.", "ReportRequest", cpptrace::generate_trace());
+        // Need to add a way to make/adjust current CalibrationResults Obj,
+        // don't have access to art obj so can't use that one currently
+        this->coms_obj_m->send_reports(report_data.get_jsoncons(),
+                                       "Calibration");
+        this->coms_obj_m->send_reports(verifcation_data.get_jsoncons(),
+                                       "Verification");
+    } catch (const std::exception &e) {
+        this->coms_obj_m->send_error("[ReportRequest] Invalid request.",
+                                     "ReportRequest",
+                                     cpptrace::generate_trace());
         return;
     }
 }
