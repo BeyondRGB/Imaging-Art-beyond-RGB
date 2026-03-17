7595f61de6437b08e4cb4c5d554ccbf9970b6944
jjalangtry <jjl4287@g.rit.edu>
2026-03-17 21:17:37 +0000

    pr feedback

--- frontend/cypress/e2e/home_page.cy.ts
+++ frontend/cypress/e2e/home_page.cy.ts
@@ -3,12 +3,12 @@ describe("Home Page", () => {
 		cy.clearLocalStorage();
 		cy.visit("/");
 
-		cy.contains("#homeContent button", "Process");
-		cy.contains("#homeContent", "Process a new RAW image set");
-		cy.contains("#homeContent button", "View");
-		cy.contains("#homeContent", "View a previously-processed imaged set");
-		cy.contains("#homeContent button", "Create Another Window");
-		cy.contains("#homeContent button", "About");
-		cy.contains("#homeContent", "About the program");
+		cy.contains("#homeContent button", "Process").should("be.visible");
+		cy.contains("#homeContent", "Process a new RAW image set").should("be.visible");
+		cy.contains("#homeContent button", "View").should("be.visible");
+		cy.contains("#homeContent", "View a previously-processed imaged set").should("be.visible");
+		cy.contains("#homeContent button", "Create Another Window").should("be.visible");
+		cy.contains("#homeContent button", "About").should("be.visible");
+		cy.contains("#homeContent", "About the program").should("be.visible");
 	});
 });
